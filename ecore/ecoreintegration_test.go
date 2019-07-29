6a3500db63168ac2fa5fb2d3d2ebd8020bec3f53
Jocelyn Dumay <jdy@masagroup.net>
2019-07-29 12:37:09 +0200

    [ecore][go] test EObject::EContents method

--- ecore/ecoreintegration_test.go
+++ ecore/ecoreintegration_test.go
@@ -14,3 +14,17 @@ func TestECoreEClasses(t *testing.T) {
 	assert.Equal(t, p.GetEOperation() , f.CreateEOperation().EClass() )
 	assert.Equal(t, p.GetEDataType() , f.CreateEDataType().EClass() )
 }
+
+func TestECoreEContents( t *testing.T) {
+	f :=  GetFactory()
+	c := f.CreateEClass()
+	f1 := f.CreateEAttribute()
+	f1.SetName("f1")
+	f2 := f.CreateEAttribute()
+	f2.SetName("f2")
+	c.GetEStructuralFeatures().AddAll( NewImmutableEList([]interface{}{f1,f2} ) )
+	o1 := f.CreateEOperation()
+	o1.SetName("o1")
+	c.GetEOperations().Add(o1)
+	assert.Equal(t, []interface{}{f1,f2,o1} , c.EContents().ToArray() )
+}

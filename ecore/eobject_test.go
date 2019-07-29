0dc75f7b50cf73f9ae97a4ac0aac693672a64a1a
Jocelyn Dumay <jdy@masagroup.net>
2019-07-29 15:08:55 +0200

    [ecore][go] test EObject::EContainingFeature

--- ecore/eobject_test.go
+++ ecore/eobject_test.go
@@ -16,12 +16,24 @@ import (
 )
 
 func TestEObjectEContents(t *testing.T) {
-	f := GetFactory()
-	c := f.CreateEClass()
-	f1 := f.CreateEAttribute()
-	f2 := f.CreateEAttribute()
-	o1 := f.CreateEOperation()
+	factory := GetFactory()
+	c := factory.CreateEClass()
+	f1 := factory.CreateEAttribute()
+	f2 := factory.CreateEAttribute()
+	o1 := factory.CreateEOperation()
 	c.GetEStructuralFeatures().AddAll(NewImmutableEList([]interface{}{f1, f2}))
 	c.GetEOperations().Add(o1)
 	assert.Equal(t, []interface{}{f1, f2, o1}, c.EContents().ToArray())
 }
+
+func TestEObjectEContainingFeature(t *testing.T) {
+	factory := GetFactory()
+	c := factory.CreateEClass()
+	f := factory.CreateEAttribute()
+	o := factory.CreateEOperation()
+	c.GetEStructuralFeatures().Add(f)
+	c.GetEOperations().Add(o)
+	assert.Equal(t, GetPackage().GetEClass_EStructuralFeatures() , f.EContainingFeature() )
+	assert.Equal(t, GetPackage().GetEClass_EOperations() , o.EContainingFeature() )
+}
+

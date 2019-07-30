f697f4f63ed74c44883614976da74401d135e33c
Jocelyn Dumay <jdy@masagroup.net>
2019-07-30 11:58:16 +0200

    [ecore][go] add some tests on basiceobject

---
+++ ecore/basiceobject_test.go
@@ -0,0 +1,42 @@
+package ecore
+
+import (
+	"testing"
+	"net/url"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBasicEObjectGetInterfaces(t *testing.T) {
+	o := NewBasicEObject()
+	assert.Equal(t , o , o.GetInterfaces() )
+}
+
+func TestBasicEObjectGetEObject(t *testing.T) {
+	o := NewBasicEObject()
+	assert.Equal(t , o , o.GetEObject() )
+}
+
+func TestBasicEObjectEClass(t *testing.T) {
+	o := NewBasicEObject()
+	assert.Equal(t , GetPackage().GetEObject() , o.EClass() )
+}
+
+func TestBasicEObjectEIsProxy(t *testing.T) {
+	o := NewBasicEObject()
+	assert.False(t , o.EIsProxy() )
+	o.ESetProxyURI( &url.URL{} )
+	assert.True(t , o.EIsProxy() )
+}
+
+func TestBasicEObjectContainer(t *testing.T) {
+	// set the container
+	o := NewBasicEObject()
+	mockObject := new(MockEObject)
+	mockResource := new(MockEResource)
+	mockObject.On("EResource").Return(mockResource)
+	mockResource.On("Attached",o)
+	mockNotifications := new(MockENotificationChain)
+	assert.Equal( t , mockNotifications, o.EBasicSetContainer( mockObject , 1, mockNotifications ) )
+	assert.Equal( t , mockObject, o.EContainer() )
+	assert.Equal( t , 1, o.EContainerFeatureID() )
+}

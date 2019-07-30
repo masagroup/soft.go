2bd7c7f21eba8cfe357cf06968d4815892e0822d
Jocelyn Dumay <jdy@masagroup.net>
2019-07-30 12:28:11 +0200

    [ecore][go] add elistiterator tests

---
+++ ecore/elistiterator_test.go
@@ -0,0 +1,20 @@
+package ecore
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEListIterator( t *testing.T) {
+	mockList := &MockEList{}
+	mockList.On("Size").Return(3)
+	for i := 0 ; i < 3 ; i++ {
+		mockList.On("Get",i).Return(i)	
+	}
+	it :=  &listIterator{ list : mockList }
+	for i := 0 ; it.HasNext(); i++ {
+		assert.Equal( t , i , it.Next() )
+	}
+	assert.Panics( t , func() { it.Next() })
+}

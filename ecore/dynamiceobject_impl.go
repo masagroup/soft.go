59f68b459a3027d5bed1620acc6a3ebd04a65699
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2019-09-18 11:43:29 +0200

    [ecore][go] format code

--- ecore/dynamiceobject_impl.go
+++ ecore/dynamiceobject_impl.go
@@ -12,7 +12,7 @@ package ecore
 // DynamicEObjectImpl ...
 type DynamicEObjectImpl struct {
 	*EObjectImpl
-	class EClass
+	class      EClass
 	properties []interface{}
 }
 
@@ -20,41 +20,39 @@ type DynamicEObjectImpl struct {
 func NewDynamicEObjectImpl() *DynamicEObjectImpl {
 	o := new(DynamicEObjectImpl)
 	o.EObjectImpl = NewEObjectImpl()
-	o.SetInterfaces( o )
+	o.SetInterfaces(o)
 	return o
 }
 
 // EClass ...
 func (o *DynamicEObjectImpl) EClass() EClass {
-	if ( o.class == nil ) {
+	if o.class == nil {
 		return o.EStaticClass()
 	}
 	return o.class
 }
 
 // SetEClass ...
-func (o *DynamicEObjectImpl) SetEClass( class EClass ) {
+func (o *DynamicEObjectImpl) SetEClass(class EClass) {
 	o.class = class
 }
 
-
-
 func (o *DynamicEObjectImpl) EGetFromID(featureID int, resolve bool, core bool) interface{} {
 	dynamicFeatureID := featureID - o.eStaticFeatureCount()
-    if dynamicFeatureID >= 0 {
-        feature := o.eDynamicFeature( featureID )
+	if dynamicFeatureID >= 0 {
+		feature := o.eDynamicFeature(featureID)
 
 		// retrieve value or compute it if empty
 		result := o.properties[dynamicFeatureID]
-        if result == nil {
+		if result == nil {
 			if feature.IsMany() {
-				result = o.createList( feature )
+				result = o.createList(feature)
 			}
-			o.properties[dynamicFeatureID] = result 
-        }
+			o.properties[dynamicFeatureID] = result
+		}
 		return result
-    }
-    return o.EObjectImpl.EGetFromID( featureID, resolve , core )
+	}
+	return o.EObjectImpl.EGetFromID(featureID, resolve, core)
 }
 
 func (o *DynamicEObjectImpl) eStaticFeatureCount() int {
@@ -65,69 +63,69 @@ func (o *DynamicEObjectImpl) eStaticOperationCount() int {
 	return o.EStaticClass().GetOperationCount()
 }
 
-func (o *DynamicEObjectImpl) eDynamicFeatureID( feature EStructuralFeature ) int {
-    return o.EClass().GetFeatureID( feature ) - o.eStaticFeatureCount()
+func (o *DynamicEObjectImpl) eDynamicFeatureID(feature EStructuralFeature) int {
+	return o.EClass().GetFeatureID(feature) - o.eStaticFeatureCount()
 }
 
-func (o *DynamicEObjectImpl) eDynamicFeature( dynamicFeatureID int  ) EStructuralFeature {
-    return o.EClass().GetEStructuralFeature( dynamicFeatureID + o.eStaticFeatureCount() )
+func (o *DynamicEObjectImpl) eDynamicFeature(dynamicFeatureID int) EStructuralFeature {
+	return o.EClass().GetEStructuralFeature(dynamicFeatureID + o.eStaticFeatureCount())
 }
 
-func (o *DynamicEObjectImpl) isBidirectional( feature EStructuralFeature ) bool {
-	ref , isRef := feature.(EReference)
+func (o *DynamicEObjectImpl) isBidirectional(feature EStructuralFeature) bool {
+	ref, isRef := feature.(EReference)
 	if isRef {
 		return ref.GetEOpposite() != nil
 	}
 	return false
 }
 
-func (o *DynamicEObjectImpl) isContainer( feature EStructuralFeature ) bool {
-	ref , isRef := feature.(EReference)
+func (o *DynamicEObjectImpl) isContainer(feature EStructuralFeature) bool {
+	ref, isRef := feature.(EReference)
 	if isRef {
 		opposite := ref.GetEOpposite()
 		if opposite != nil {
 			return opposite.IsContainment()
 		}
 	}
-    return false
+	return false
 }
 
-func (o *DynamicEObjectImpl) isContains( feature EStructuralFeature ) bool {
-	ref , isRef := feature.(EReference)
+func (o *DynamicEObjectImpl) isContains(feature EStructuralFeature) bool {
+	ref, isRef := feature.(EReference)
 	if isRef {
 		return ref.IsContainment()
 	}
-    return false
+	return false
 }
 
-func (o *DynamicEObjectImpl) isBackReference( feature EStructuralFeature ) bool {
-	ref , isRef := feature.(EReference)
+func (o *DynamicEObjectImpl) isBackReference(feature EStructuralFeature) bool {
+	ref, isRef := feature.(EReference)
 	if isRef {
 		return ref.IsContainer()
 	}
-    return false
+	return false
 }
 
-func (o *DynamicEObjectImpl) isProxy( feature EStructuralFeature ) bool {
-	if o.isContainer(feature) || o.isContains( feature ) {
+func (o *DynamicEObjectImpl) isProxy(feature EStructuralFeature) bool {
+	if o.isContainer(feature) || o.isContains(feature) {
 		return false
 	}
-	
-	ref , isRef := feature.(EReference)
+
+	ref, isRef := feature.(EReference)
 	if isRef {
 		return ref.IsResolveProxies()
 	}
-    return false
+	return false
 }
 
-func (o *DynamicEObjectImpl) createList( feature EStructuralFeature ) EList {
-	if attribute , isAttribute := feature.(EAttribute); isAttribute {
+func (o *DynamicEObjectImpl) createList(feature EStructuralFeature) EList {
+	if attribute, isAttribute := feature.(EAttribute); isAttribute {
 		if attribute.IsUnique() {
 			return NewUniqueArrayEList(nil)
 		} else {
 			return NewArrayEList(nil)
 		}
-	} else if ref , isRef := feature.(EReference); isRef {
+	} else if ref, isRef := feature.(EReference); isRef {
 		inverse := false
 		opposite := false
 		reverseID := -1
@@ -140,7 +138,7 @@ func (o *DynamicEObjectImpl) createList( feature EStructuralFeature ) EList {
 			inverse = true
 			opposite = false
 		}
-		return NewEObjectEList( o.GetEObjectInternal() , ref.GetFeatureID() , reverseID , ref.IsContainment(), inverse, opposite, ref.EIsProxy(), ref.IsUnsettable() )
+		return NewEObjectEList(o.GetEObjectInternal(), ref.GetFeatureID(), reverseID, ref.IsContainment(), inverse, opposite, ref.EIsProxy(), ref.IsUnsettable())
 	}
 	return nil
 }

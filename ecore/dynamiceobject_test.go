048fa326941dfe71a64d10c551833c0e0905e5a0
Jocelyn Dumay <jdy@masagroup.net>
2019-09-19 18:20:03 +0200

    [ecore][go] add test on dynamicobject getter / setter

--- ecore/dynamiceobject_test.go
+++ ecore/dynamiceobject_test.go
@@ -15,20 +15,42 @@ import (
 	"testing"
 )
 
-func TestDynamicEObjectConstrcutor(t *testing.T) {
+func TestDynamicEObjectConstructor(t *testing.T) {
 	o := NewDynamicEObjectImpl()
-	assert.NotNil(t,o)
-	assert.Equal(t,GetPackage().GetEObject(), o.EClass() )
+	assert.NotNil(t, o)
+	assert.Equal(t, GetPackage().GetEObject(), o.EClass())
 }
 
-func TestDynamicEObjectWithMockClass(t *testing.T) {
+func TestDynamicEObject_EClass(t *testing.T) {
 	o := NewDynamicEObjectImpl()
 	mockClass := &MockEClass{}
 	mockAdapters := &MockEList{}
 	mockClass.On("GetFeatureCount").Return(0)
 	mockClass.On("EAdapters").Return(mockAdapters)
-	mockAdapters.On("Add",mock.Anything).Return(true).Once()
+	mockAdapters.On("Add", mock.Anything).Return(true).Once()
 	o.SetEClass(mockClass)
+	assert.Equal(t, mockClass, o.EClass())
 }
 
+func TestDynamicEObject_MockEClass(t *testing.T) {
+	o := NewDynamicEObjectImpl()
+	c := GetFactory().CreateEClass()
+	o.SetEClass(c)
+	assert.Equal(t, c, o.EClass())
+}
 
+func TestDynamicEObject_GetSet(t *testing.T) {
+	o := NewDynamicEObjectImpl()
+	c := GetFactory().CreateEClass()
+	o.SetEClass(c)
+	assert.Equal(t, c, o.EClass())
+
+	f := GetFactory().CreateEAttribute()
+	c.GetEStructuralFeatures().Add(f)
+	l := c.GetEAllStructuralFeatures()
+	assert.True(t, l.Contains(f))
+	assert.Nil(t, o.EGet(f))
+
+	o.ESet(f, 1)
+	assert.Equal(t, 1, o.EGet(f))
+}

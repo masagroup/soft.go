997a59f8c08ae54729ac875af7531364ada659a3
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2019-10-22 14:41:42 +0200

    [ecore][go] implements GetURI method in ecoreutils

--- ecore/ecoreutils.go
+++ ecore/ecoreutils.go
@@ -1,5 +1,10 @@
 package ecore
 
+import (
+	"net/url"
+	"strings"
+)
+
 func GetEObjectID(eObject EObject) string {
 	eClass := eObject.EClass()
 	eIDAttribute := eClass.GetEIDAttribute()
@@ -30,4 +35,53 @@ func convertToString(eDataType EDataType, value interface{}) string {
 func createFromString(eDataType EDataType, literal string) interface{} {
 	eFactory := eDataType.GetEPackage().GetEFactoryInstance()
 	return eFactory.CreateFromString(eDataType, literal)
+}
+
+func GetURI(eObject EObject) *url.URL {
+	if eObject.EIsProxy() {
+		return eObject.(EObjectInternal).EProxyURI()
+	} else {
+		resource := eObject.EResource()
+		if resource != nil {
+			uri := resource.GetURI()
+			uri.Fragment = resource.GetURIFragment(eObject)
+			return uri
+		} else {
+			id := GetEObjectID(eObject)
+			if len(id) == 0 {
+				return &url.URL{Fragment: getRelativeURIFragmentPath(nil, eObject, false)}
+			} else {
+				return &url.URL{Fragment: id}
+			}
+		}
+	}
+}
+
+func getRelativeURIFragmentPath(ancestor EObject, descendant EObject, resolve bool) string {
+	if ancestor == descendant {
+		return ""
+	}
+	eObject := descendant
+	eContainer := eObject.EContainer()
+	visited := make(map[EObject]bool)
+	fragmentPath := []string{}
+	for {
+		if eContainer == nil {
+			break
+		}
+		if _, v := visited[eObject]; v {
+			break
+		}
+		fragmentPath = append([]string{eContainer.(EObjectInternal).EURIFragmentSegment(eObject.EContainingFeature(), eObject)}, fragmentPath...)
+		eObject = eContainer
+		if eContainer == ancestor {
+			break
+		}
+		eContainer = eObject.EContainer()
+	}
+	if eObject != ancestor && ancestor != nil {
+		panic("The ancestor not found")
+	}
+
+	return strings.Join(fragmentPath, "/")
 }

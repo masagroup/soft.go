bafc7e21e591f61a249ae61a2ef210d510b60d2b
Jocelyn DUMAY <jocelyn.dumay@masagroup.net>
2019-10-29 10:07:03 +0100

    [ecore][go] add EResourceSet getResource and getEObject tests

--- ecore/eresourceset_impl_test.go
+++ ecore/eresourceset_impl_test.go
@@ -43,3 +43,38 @@ func TestEResourceSetCreateResource(t *testing.T) {
 	mockResource.On("basicSetResourceSet", rs, nil).Return(nil)
 	assert.NotNil(t, mockResource, rs.CreateResource(uri))
 }
+
+func TestEResourceSetGetResource(t *testing.T) {
+	mockResourceFactoryRegistry := new(MockEResourceFactoryRegistry)
+	mockResourceFactory := new(MockEResourceFactory)
+	mockResource := new(MockEResourceInternal)
+	uri, _ := url.Parse("test://file.t")
+	rs := NewEResourceSetImpl()
+	rs.SetResourceFactoryRegistry(mockResourceFactoryRegistry)
+
+	mockResourceFactoryRegistry.On("GetFactory", uri).Return(mockResourceFactory)
+	mockResourceFactory.On("CreateResource", uri).Return(mockResource)
+	mockResource.On("basicSetResourceSet", rs, nil).Return(nil)
+	mockResource.On("Load").Once()
+
+	assert.Equal(t, mockResource, rs.GetResource(uri, true))
+}
+
+func TestEResourceSetGetEObject(t *testing.T) {
+	mockResourceFactoryRegistry := new(MockEResourceFactoryRegistry)
+	mockResourceFactory := new(MockEResourceFactory)
+	mockResource := new(MockEResourceInternal)
+	mockObject := new(MockEObject)
+	uriObject, _ := url.Parse("test://file.t#//@first/second")
+	uriResource, _ := url.Parse("test://file.t")
+	rs := NewEResourceSetImpl()
+	rs.SetResourceFactoryRegistry(mockResourceFactoryRegistry)
+
+	mockResourceFactoryRegistry.On("GetFactory", uriResource).Return(mockResourceFactory)
+	mockResourceFactory.On("CreateResource", uriResource).Return(mockResource)
+	mockResource.On("basicSetResourceSet", rs, nil).Return(nil)
+	mockResource.On("Load").Once()
+	mockResource.On("GetEObject", "//@first/second").Return(mockObject)
+
+	assert.Equal(t, mockObject, rs.GetEObject(uriObject, true))
+}

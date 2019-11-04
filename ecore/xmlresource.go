04108b9b221c12683e2023bc1658ead17a7349c5
Jocelyn DUMAY <jocelyn.dumay@masagroup.net>
2019-11-04 12:22:44 +0100

    [ecore][go] add xml namespace support struct and api

--- ecore/xmlresource.go
+++ ecore/xmlresource.go
@@ -8,8 +8,81 @@ import (
 	"golang.org/x/net/html/charset"
 )
 
-type XMLResource struct {
-	*EResourceImpl
+type pair [2]interface{}
+
+type xmlNamespaces struct {
+	namespaces     []pair
+	namespacesSize int
+	currentContext int
+	contexts       []int
+}
+
+func newXmlNamespaces() *xmlNamespaces {
+	n := &xmlNamespaces{
+		currentContext: -1,
+	}
+	return n
+}
+
+func (n *xmlNamespaces) pushContext() {
+	n.currentContext++
+	if n.currentContext >= len(n.contexts) {
+		n.contexts = append(n.contexts, n.namespacesSize)
+	} else {
+		n.contexts[n.currentContext] = n.namespacesSize
+	}
+}
+
+func (n *xmlNamespaces) popContext() []pair {
+	oldPrefixSize := n.namespacesSize
+	n.namespacesSize = n.contexts[n.currentContext]
+	n.currentContext--
+	return n.namespaces[n.namespacesSize:oldPrefixSize]
+}
+
+func (n *xmlNamespaces) declarePrefix(prefix string, uri string) bool {
+	for i := n.namespacesSize; i > n.contexts[n.currentContext]; i-- {
+		p := &n.namespaces[i-1]
+		if p[0] == prefix {
+			p[1] = uri
+			return true
+		}
+	}
+	n.namespacesSize++
+	if n.namespacesSize > len(n.namespaces) {
+		n.namespaces = append(n.namespaces, pair{prefix, uri})
+	} else {
+		n.namespaces[n.namespacesSize] = pair{prefix, uri}
+	}
+	return false
+}
+
+func (n *xmlNamespaces) getPrefix(uri string) string {
+	for i := n.namespacesSize; i > 0; i-- {
+		p := n.namespaces[i-1]
+		if p[1].(string) == uri {
+			return p[0].(string)
+		}
+	}
+	return ""
+}
+
+func (n *xmlNamespaces) getURI(prefix string) string {
+	for i := n.namespacesSize; i > 0; i-- {
+		p := n.namespaces[i-1]
+		if p[0].(string) == prefix {
+			return p[1].(string)
+		}
+	}
+	return ""
+}
+
+type xmlResourceLoader struct {
+	resource      EResource
+	isRoot        bool
+	isPushContext bool
+	elements      []string
+	objects       []interface{}
 }
 
 func NewXMLResource() *XMLResource {
@@ -19,7 +92,19 @@ func NewXMLResource() *XMLResource {
 	return r
 }
 
+type XMLResource struct {
+	*EResourceImpl
+}
+
 func (r *XMLResource) DoLoad(rd io.Reader) {
+	// loader := &xmlResourceLoader{
+	// 	resource:      r,
+	// 	isRoot:        true,
+	// 	isPushContext: true,
+	// 	elements:      []string{},
+	// 	objects:       []interface{}{},
+	// }
+
 	d := xml.NewDecoder(rd)
 	d.CharsetReader = charset.NewReaderLabel
 	for {
--- ecore/xmlresource_test.go
+++ ecore/xmlresource_test.go
@@ -7,6 +7,67 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestXmlNamespacesNoContext(t *testing.T) {
+	n := newXmlNamespaces()
+	assert.Equal(t, "", n.getURI("prefix"))
+	assert.Equal(t, "", n.getPrefix("uri"))
+}
+
+func TestXmlNamespacesEmpty(t *testing.T) {
+	n := newXmlNamespaces()
+	n.pushContext()
+	assert.Equal(t, "", n.getURI("prefix"))
+	assert.Equal(t, "", n.getPrefix("uri"))
+	c := n.popContext()
+	assert.Equal(t, 0, len(c))
+}
+
+func TestXmlNamespacesContext(t *testing.T) {
+	n := newXmlNamespaces()
+	n.pushContext()
+	assert.False(t, n.declarePrefix("prefix", "uri"))
+	assert.Equal(t, "uri", n.getURI("prefix"))
+	assert.Equal(t, "prefix", n.getPrefix("uri"))
+
+	n.pushContext()
+	assert.False(t, n.declarePrefix("prefix", "uri2"))
+	assert.Equal(t, "uri2", n.getURI("prefix"))
+	assert.Equal(t, "prefix", n.getPrefix("uri2"))
+
+	n.popContext()
+	assert.Equal(t, "uri", n.getURI("prefix"))
+	assert.Equal(t, "prefix", n.getPrefix("uri"))
+
+	n.popContext()
+	assert.Equal(t, "", n.getURI("prefix"))
+	assert.Equal(t, "", n.getPrefix("uri"))
+}
+
+func TestXmlNamespacesContextRemap(t *testing.T) {
+	n := newXmlNamespaces()
+	n.pushContext()
+	assert.False(t, n.declarePrefix("prefix", "uri"))
+	assert.Equal(t, "uri", n.getURI("prefix"))
+	assert.Equal(t, "prefix", n.getPrefix("uri"))
+
+	assert.True(t, n.declarePrefix("prefix", "uri2"))
+	assert.Equal(t, "uri2", n.getURI("prefix"))
+	assert.Equal(t, "prefix", n.getPrefix("uri2"))
+}
+
+func TestXmlNamespacesContextNoRemap(t *testing.T) {
+	n := newXmlNamespaces()
+	n.pushContext()
+	assert.False(t, n.declarePrefix("prefix", "uri"))
+	assert.Equal(t, "uri", n.getURI("prefix"))
+	assert.Equal(t, "prefix", n.getPrefix("uri"))
+
+	n.pushContext()
+	assert.False(t, n.declarePrefix("prefix", "uri2"))
+	assert.Equal(t, "uri2", n.getURI("prefix"))
+	assert.Equal(t, "prefix", n.getPrefix("uri2"))
+}
+
 func TestXMLResourceLoad(t *testing.T) {
 	resource := NewXMLResource()
 	resource.SetURI(&url.URL{Path: "testdata/simple.book.ecore"})

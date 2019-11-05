f1bdd17372c4e80e062deec86e1b4d05d80bb5ec
Jocelyn DUMAY <jocelyn.dumay@masagroup.net>
2019-11-05 10:37:04 +0100

    [ecore][go] add diagnostic implementation  class

---
+++ ecore/ediagnostic_impl.go
@@ -0,0 +1,33 @@
+package ecore
+
+type EDiagnosticImpl struct {
+	message  string
+	location string
+	line     int
+	column   int
+}
+
+func newEDiagnosticImpl(message string, location string, line int, column int) *EDiagnosticImpl {
+	return &EDiagnosticImpl{
+		message:  message,
+		location: location,
+		line:     line,
+		column:   column,
+	}
+}
+
+func (d *EDiagnosticImpl) GetMessage() string {
+	return d.message
+}
+
+func (d *EDiagnosticImpl) GetLocation() string {
+	return d.location
+}
+
+func (d *EDiagnosticImpl) GetLine() int {
+	return d.line
+}
+
+func (d *EDiagnosticImpl) GetColumn() int {
+	return d.column
+}

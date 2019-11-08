8f78b7ed7afa9764ee244935b4f8548a218bb065
Jocelyn DUMAY <jocelyn.dumay@masagroup.net>
2019-11-08 15:41:00 +0100

    [ecore][go] EPackageExt.GetClassifier method implementation

--- ecore/epackage_ext.go
+++ ecore/epackage_ext.go
@@ -9,14 +9,44 @@
 
 package ecore
 
+type ePackageExtAdapter struct {
+	*Adapter
+	pack *EPackageExt
+}
+
+func (a *ePackageExtAdapter) NotifyChanged(notification ENotification) {
+	eventType := notification.GetEventType()
+	if eventType != REMOVING_ADAPTER {
+		featureID := notification.GetFeatureID()
+		if featureID == EPACKAGE__ECLASSIFIERS {
+			a.pack.nameToClassifier = nil
+		}
+	}
+}
+
 // EPackageExt is the extension of the model object 'EFactory'
 type EPackageExt struct {
 	*ePackageImpl
+	adapter          EAdapter
+	nameToClassifier map[string]EClassifier
 }
 
 func NewEPackageExt() *EPackageExt {
-	ePackage := new(EPackageExt)
-	ePackage.ePackageImpl = newEPackageImpl()
-	ePackage.SetInterfaces(ePackage)
-	return ePackage
+	pack := new(EPackageExt)
+	pack.ePackageImpl = newEPackageImpl()
+	pack.adapter = &ePackageExtAdapter{Adapter: NewAdapter(), pack: pack}
+	pack.SetInterfaces(pack)
+	pack.EAdapters().Add(pack.adapter)
+	return pack
+}
+
+func (pack *EPackageExt) GetEClassifier(classifier string) EClassifier {
+	if pack.nameToClassifier == nil {
+		pack.nameToClassifier = make(map[string]EClassifier)
+		for itClassifier := pack.GetEClassifiers().Iterator(); itClassifier.HasNext(); {
+			classifier := itClassifier.Next().(EClassifier)
+			pack.nameToClassifier[classifier.GetName()] = classifier
+		}
+	}
+	return pack.nameToClassifier[classifier]
 }

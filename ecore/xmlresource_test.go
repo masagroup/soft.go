c14ebb0e52d60c66165ff7e02ad7b9463285cf57
Jocelyn DUMAY <jocelyn.dumay@masagroup.net>
2019-11-15 15:20:13 +0100

    [ecore][go] test xml load structure

--- ecore/xmlresource_test.go
+++ ecore/xmlresource_test.go
@@ -75,4 +75,61 @@ func TestXMLResourceLoad(t *testing.T) {
 	assert.True(t, resource.IsLoaded())
 	assert.True(t, resource.GetErrors().Empty())
 	assert.True(t, resource.GetWarnings().Empty())
+
+	contents := resource.GetContents()
+	assert.Equal(t, 1, contents.Size())
+
+	ePackage, _ := contents.Get(0).(EPackage)
+	assert.NotNil(t, ePackage)
+	assert.Equal(t, "BookStorePackage", ePackage.GetName())
+	assert.Equal(t, "bookStore", ePackage.GetNsPrefix())
+	assert.Equal(t, "http:///com.ibm.dynamic.example.bookStore.ecore", ePackage.GetNsURI())
+
+	eClassifiers := ePackage.GetEClassifiers()
+	assert.Equal(t, 2, eClassifiers.Size())
+
+	eBookStore, _ := eClassifiers.Get(0).(EClassifier)
+	assert.NotNil(t, eBookStore)
+	assert.Equal(t, "BookStore", eBookStore.GetName())
+
+	eBookStoreClass, _ := eBookStore.(EClass)
+	assert.NotNil(t, eBookStoreClass)
+	assert.Equal(t, 3, eBookStoreClass.GetFeatureCount())
+
+	eOwnerFeature := eBookStoreClass.GetEStructuralFeature(0)
+	assert.Equal(t, "owner", eOwnerFeature.GetName())
+	eOwnerAttribute, _ := eOwnerFeature.(EAttribute)
+	assert.NotNil(t, eOwnerAttribute)
+
+	eLocationFeature := eBookStoreClass.GetEStructuralFeature(1)
+	assert.Equal(t, "location", eLocationFeature.GetName())
+	eLocationAttribute, _ := eLocationFeature.(EAttribute)
+	assert.NotNil(t, eLocationAttribute)
+	eLocationType := eLocationAttribute.GetEAttributeType()
+	assert.NotNil(t, eLocationType)
+
+	eBooksFeature := eBookStoreClass.GetEStructuralFeature(2)
+	assert.Equal(t, "books", eBooksFeature.GetName())
+	eBooksReference, _ := eBooksFeature.(EReference)
+	assert.NotNil(t, eBooksReference)
+
+	eBook := eClassifiers.Get(1).(EClassifier)
+	assert.NotNil(t, eBook)
+	assert.Equal(t, "Book", eBook.GetName())
+	eBookClass, _ := eBook.(EClass)
+	assert.NotNil(t, eBookClass)
+	assert.Equal(t, 2, eBookClass.GetFeatureCount())
+
+	eNameFeature := eBookClass.GetEStructuralFeature(0)
+	assert.Equal(t, "name", eNameFeature.GetName())
+	eNameAttribute, _ := eNameFeature.(EAttribute)
+	assert.NotNil(t, eNameAttribute)
+
+	eISBNFeature := eBookClass.GetEStructuralFeature(1)
+	assert.Equal(t, "isbn", eISBNFeature.GetName())
+	eISBNAttribute, _ := eISBNFeature.(EAttribute)
+	assert.NotNil(t, eISBNAttribute)
+
+	// check resolved reference
+	assert.Equal(t, eBookClass, eBooksReference.GetEReferenceType())
 }

efb23d25595c13311fcfef44088e84ebc5f99e7d
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-03-23 12:50:50 +0100

    [library][go] add a loading test for a library file sample

---
+++ library/serialization_test.go
@@ -0,0 +1,21 @@
+package library
+
+import (
+	"net/url"
+	"testing"
+
+	"github.com/masagroup/soft/go/ecore"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSerializationXML(t *testing.T) {
+	ecore.GetPackageRegistry().RegisterPackage(GetPackage())
+
+	fileURI := &url.URL{Path: "testdata/library.xml"}
+	resourceFactory := ecore.GetResourceFactoryRegistry().GetFactory(fileURI)
+	resource := resourceFactory.CreateResource(fileURI)
+	resource.Load()
+	assert.True(t, resource.IsLoaded())
+	assert.True(t, resource.GetErrors().Empty())
+	assert.True(t, resource.GetWarnings().Empty())
+}

beea9fb59062c45b274ee91264ab59667619cc93
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-04-01 22:29:55 +0200

    [ecore][go] EList::Set returns old value

--- ecore/elist_mock.go
+++ ecore/elist_mock.go
@@ -196,8 +196,19 @@ func (_m *MockEList) RemoveAt(_a0 int) interface{} {
 }
 
 // Set provides a mock function with given fields: _a0, _a1
-func (_m *MockEList) Set(_a0 int, _a1 interface{}) {
-	_m.Called(_a0, _a1)
+func (_m *MockEList) Set(_a0 int, _a1 interface{}) interface{} {
+	ret := _m.Called(_a0, _a1)
+
+	var r0 interface{}
+	if rf, ok := ret.Get(0).(func(int, interface{}) interface{}); ok {
+		r0 = rf(_a0, _a1)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(interface{})
+		}
+	}
+
+	return r0
 }
 
 // Size provides a mock function with given fields:

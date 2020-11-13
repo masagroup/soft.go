70fb18cb3fc4a464fe898c26fb1dff429fb72105
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-11-13 12:36:10 +0100

    [ecore][go] test reflectiveeobjectimpl accessors in a container reference

--- ecore/reflectiveeobject_impl_test.go
+++ ecore/reflectiveeobject_impl_test.go
@@ -18,6 +18,7 @@ func TestReflectiveEObjectImpl_GetAttribute(t *testing.T) {
 	o := NewReflectiveEObjectImpl()
 	o.setEClass(mockClass)
 
+	// get unitialized value
 	assert.Nil(t, o.EGetFromID(0, true))
 
 	mock.AssertExpectationsForObjects(t, mockClass, mockAttribute)
@@ -35,8 +36,10 @@ func TestReflectiveEObjectImpl_SetAttribute(t *testing.T) {
 	o := NewReflectiveEObjectImpl()
 	o.setEClass(mockClass)
 
+	// set
 	o.ESetFromID(0, mockObject)
 
+	// check that value is well set
 	assert.Equal(t, mockObject, o.EGetFromID(0, true))
 
 	mock.AssertExpectationsForObjects(t, mockClass, mockAttribute)
@@ -55,10 +58,64 @@ func TestReflectiveEObjectImpl_UnsetAttribute(t *testing.T) {
 	o := NewReflectiveEObjectImpl()
 	o.setEClass(mockClass)
 
+	// set - unset
 	o.ESetFromID(0, mockObject)
 	o.EUnsetFromID(0)
 
+	// check that value is unset
 	assert.Nil(t, o.EGetFromID(0, true))
 
 	mock.AssertExpectationsForObjects(t, mockClass, mockAttribute)
+}
+
+func TestReflectiveEObjectImpl_GetContainer(t *testing.T) {
+	mockOpposite := new(MockEReference)
+	mockOpposite.On("IsContainment").Return(true).Once()
+
+	mockReference := new(MockEReference)
+	mockReference.On("GetEOpposite").Return(mockOpposite).Once()
+
+	mockClass := new(MockEClass)
+	mockClass.On("GetEStructuralFeature", 0).Return(mockReference).Once()
+	mockClass.On("GetFeatureID", mockReference).Return(0).Once()
+
+	o := NewReflectiveEObjectImpl()
+	o.setEClass(mockClass)
+
+	// get non initialized container
+	assert.Nil(t, o.EGetFromID(0, true))
+
+	mock.AssertExpectationsForObjects(t, mockClass, mockReference, mockOpposite)
+}
+
+func TestReflectiveEObjectImpl_SetContainer(t *testing.T) {
+	mockObject := new(MockEObjectInternal)
+	mockObject.On("EResource").Return(nil).Once()
+	mockObject.On("EIsProxy").Return(false).Once()
+
+	mockOpposite := new(MockEReference)
+	mockOpposite.On("IsContainment").Return(true).Times(3)
+
+	mockReference := new(MockEReference)
+	mockReference.On("GetEOpposite").Return(mockOpposite).Times(3)
+
+	mockClass := new(MockEClass)
+	mockClass.On("GetEStructuralFeature", 0).Return(mockReference).Times(3)
+	mockClass.On("GetFeatureID", mockReference).Return(0).Times(3)
+
+	o := NewReflectiveEObjectImpl()
+	o.setEClass(mockClass)
+
+	mockObject.On("EInverseAdd", o.GetInterfaces(), 0, nil).Return(nil)
+
+	// set object reference as container
+	o.ESetFromID(0, mockObject)
+
+	// get unresolved
+	assert.Equal(t, mockObject, o.EGetFromID(0, false))
+
+	// get resolved
+	assert.Equal(t, mockObject, o.EGetFromID(0, true))
+
+	mock.AssertExpectationsForObjects(t, mockClass, mockReference, mockOpposite, mockObject)
 }

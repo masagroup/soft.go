437acf66a3cb23537b320d43933a2946e0e44d4e
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-11-16 14:06:38 +0100

    [ecore][go] test EStoreEObjectImpl get with attribute

---
+++ ecore/estoreeobject_impl_test.go
@@ -0,0 +1,75 @@
+package ecore
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type EStoreEObjectTest struct {
+	*EStoreEObjectImpl
+	mockStore *MockEStore
+}
+
+func newEStoreEObjectTest(isCaching bool) *EStoreEObjectTest {
+	o := new(EStoreEObjectTest)
+	o.mockStore = new(MockEStore)
+	o.EStoreEObjectImpl = NewEStoreEObjectImpl(isCaching)
+	o.SetInterfaces(o)
+	return o
+}
+
+func (o *EStoreEObjectTest) EStore() EStore {
+	return o.mockStore
+}
+
+func TestEStoreEObjectImpl_GetAttribute_Caching(t *testing.T) {
+	// create object
+	o := newEStoreEObjectTest(true)
+
+	// create mocks
+	mockClass := new(MockEClass)
+	mockAttribute := new(MockEAttribute)
+	mockStore := o.mockStore
+
+	// initialise object with mock class
+	o.setEClass(mockClass)
+
+	// first get
+	mockAttribute.On("IsMany").Return(false).Once()
+	mockAttribute.On("IsTransient").Return(false).Once()
+	mockClass.On("GetFeatureCount").Return(1).Once()
+	mockClass.On("GetEStructuralFeature", 0).Return(mockAttribute).Twice()
+	mockStore.On("Get", o, mockAttribute, NO_INDEX).Return(2).Once()
+	assert.Equal(t, 2, o.EGetFromID(0, false))
+	mock.AssertExpectationsForObjects(t, mockClass, mockAttribute, mockStore)
+
+	// second - test caching
+	mockClass.On("GetEStructuralFeature", 0).Return(mockAttribute).Once()
+	assert.Equal(t, 2, o.EGetFromID(0, false))
+	mock.AssertExpectationsForObjects(t, mockClass, mockAttribute, mockStore)
+}
+
+func TestEStoreEObjectImpl_GetAttribute_NoCaching(t *testing.T) {
+	// create object
+	o := newEStoreEObjectTest(false)
+
+	// create mocks
+	mockClass := new(MockEClass)
+	mockAttribute := new(MockEAttribute)
+	mockStore := o.mockStore
+
+	// initialise object with mock class
+	o.setEClass(mockClass)
+
+	mockClass.On("GetFeatureCount").Return(1).Once()
+	for i := 0; i < 2; i++ {
+		mockAttribute.On("IsMany").Return(false).Once()
+		mockAttribute.On("IsTransient").Return(false).Once()
+		mockClass.On("GetEStructuralFeature", 0).Return(mockAttribute).Twice()
+		mockStore.On("Get", o, mockAttribute, NO_INDEX).Return(2).Once()
+		assert.Equal(t, 2, o.EGetFromID(0, false))
+		mock.AssertExpectationsForObjects(t, mockClass, mockAttribute, mockStore)
+	}
+}

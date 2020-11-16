ab81fc4eebd8c250c40f38b99bdbd56156a2ebb8
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-11-16 14:29:48 +0100

    [ecore][go] test EStoreEObjectImpl accesors with a transient attribute

--- ecore/estoreeobject_impl_test.go
+++ ecore/estoreeobject_impl_test.go
@@ -24,6 +24,27 @@ func (o *EStoreEObjectTest) EStore() EStore {
 	return o.mockStore
 }
 
+func TestEStoreEObjectImpl_GetAttribute_Transient(t *testing.T) {
+	// create object
+	o := newEStoreEObjectTest(true)
+
+	// create mocks
+	mockClass := new(MockEClass)
+	mockAttribute := new(MockEAttribute)
+	mockStore := o.mockStore
+
+	// initialise object with mock class
+	o.setEClass(mockClass)
+
+	// first get
+	mockAttribute.On("IsMany").Return(false).Once()
+	mockAttribute.On("IsTransient").Return(true).Once()
+	mockClass.On("GetFeatureCount").Return(1).Once()
+	mockClass.On("GetEStructuralFeature", 0).Return(mockAttribute).Twice()
+	assert.Nil(t, o.EGetFromID(0, false))
+	mock.AssertExpectationsForObjects(t, mockClass, mockAttribute, mockStore)
+}
+
 func TestEStoreEObjectImpl_GetAttribute_Caching(t *testing.T) {
 	// create object
 	o := newEStoreEObjectTest(true)
@@ -74,6 +95,31 @@ func TestEStoreEObjectImpl_GetAttribute_NoCaching(t *testing.T) {
 	}
 }
 
+func TestEStoreEObjectImpl_SetAttribute_Transient(t *testing.T) {
+	// create object
+	o := newEStoreEObjectTest(true)
+
+	// create mocks
+	mockClass := new(MockEClass)
+	mockAttribute := new(MockEAttribute)
+	mockStore := o.mockStore
+
+	// initialise object with mock class
+	o.setEClass(mockClass)
+
+	// set
+	mockAttribute.On("IsTransient").Return(true).Twice()
+	mockClass.On("GetFeatureCount").Return(1).Once()
+	mockClass.On("GetEStructuralFeature", 0).Return(mockAttribute).Times(3)
+	o.ESetFromID(0, 2)
+	mock.AssertExpectationsForObjects(t, mockClass, mockAttribute, mockStore)
+
+	// test get
+	mockClass.On("GetFeatureCount").Return(1).Once()
+	mockClass.On("GetEStructuralFeature", 0).Return(mockAttribute).Twice()
+	assert.Equal(t, 2, o.EGetFromID(0, false))
+}
+
 func TestEStoreEObjectImpl_SetAttribute(t *testing.T) {
 
 	// create object
@@ -95,6 +141,25 @@ func TestEStoreEObjectImpl_SetAttribute(t *testing.T) {
 	mockStore.On("Set", o, mockAttribute, NO_INDEX, 2).Once()
 	o.ESetFromID(0, 2)
 	mock.AssertExpectationsForObjects(t, mockClass, mockAttribute, mockStore)
+}
+
+func TestEStoreEObjectImpl_UnSetAttribute_Transient(t *testing.T) {
+	// create object
+	o := newEStoreEObjectTest(false)
+
+	// create mocks
+	mockClass := new(MockEClass)
+	mockAttribute := new(MockEAttribute)
+
+	// initialise object with mock class
+	o.setEClass(mockClass)
+
+	mockAttribute.On("IsMany").Return(false).Once()
+	mockAttribute.On("IsTransient").Return(true).Twice()
+	mockClass.On("GetFeatureCount").Return(1).Once()
+	mockClass.On("GetEStructuralFeature", 0).Return(mockAttribute).Times(3)
+	o.EUnsetFromID(0)
+
 }
 
 func TestEStoreEObjectImpl_UnSetAttribute(t *testing.T) {

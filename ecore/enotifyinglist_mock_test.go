9588c7663c036110e24c8e7ed7394b937bf9193b
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-11-30 14:33:45 +0100

    [ecore][go] enotifyinglist mock test

---
+++ ecore/enotifyinglist_mock_test.go
@@ -0,0 +1,82 @@
+package ecore
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestMockENotifyingListGetNotifier(t *testing.T) {
+	l := &MockENotifyingList{}
+	n := &MockENotifier{}
+	l.On("GetNotifier").Return(n).Once()
+	l.On("GetNotifier").Return(func() ENotifier {
+		return n
+	}).Once()
+	assert.Equal(t, n, l.GetNotifier())
+	assert.Equal(t, n, l.GetNotifier())
+	mock.AssertExpectationsForObjects(t, l, n)
+}
+
+func TestMockEPackageRegistryGetFeature(t *testing.T) {
+	l := &MockENotifyingList{}
+	f := &MockEStructuralFeature{}
+	l.On("GetFeature").Return(f).Once()
+	l.On("GetFeature").Return(func() EStructuralFeature {
+		return f
+	}).Once()
+	assert.Equal(t, f, l.GetFeature())
+	assert.Equal(t, f, l.GetFeature())
+	mock.AssertExpectationsForObjects(t, l, f)
+}
+
+func TestMockEPackageRegistryGetFeatureID(t *testing.T) {
+	l := &MockENotifyingList{}
+	l.On("GetFeatureID").Return(1).Once()
+	l.On("GetFeatureID").Return(func() int {
+		return 2
+	}).Once()
+	assert.Equal(t, 1, l.GetFeatureID())
+	assert.Equal(t, 2, l.GetFeatureID())
+	mock.AssertExpectationsForObjects(t, l)
+}
+
+func TestMockEPackageRegistryAddWithNotification(t *testing.T) {
+	l := &MockENotifyingList{}
+	n := &MockENotificationChain{}
+	v := &MockEObject{}
+	l.On("AddWithNotification", v, n).Return(n).Once()
+	l.On("AddWithNotification", v, n).Return(func(object interface{}, notifications ENotificationChain) ENotificationChain {
+		return n
+	}).Once()
+	assert.Equal(t, n, l.AddWithNotification(v, n))
+	assert.Equal(t, n, l.AddWithNotification(v, n))
+	mock.AssertExpectationsForObjects(t, l, n, v)
+}
+
+func TestMockEPackageRegistryRemoveWithNotification(t *testing.T) {
+	l := &MockENotifyingList{}
+	n := &MockENotificationChain{}
+	v := &MockEObject{}
+	l.On("RemoveWithNotification", v, n).Return(n).Once()
+	l.On("RemoveWithNotification", v, n).Return(func(object interface{}, notifications ENotificationChain) ENotificationChain {
+		return n
+	}).Once()
+	assert.Equal(t, n, l.RemoveWithNotification(v, n))
+	assert.Equal(t, n, l.RemoveWithNotification(v, n))
+	mock.AssertExpectationsForObjects(t, l, n, v)
+}
+
+func TestMockEPackageRegistrySetWithNotification(t *testing.T) {
+	l := &MockENotifyingList{}
+	n := &MockENotificationChain{}
+	v := &MockEObject{}
+	l.On("SetWithNotification", 0, v, n).Return(n).Once()
+	l.On("SetWithNotification", 1, v, n).Return(func(index int, object interface{}, notifications ENotificationChain) ENotificationChain {
+		return n
+	}).Once()
+	assert.Equal(t, n, l.SetWithNotification(0, v, n))
+	assert.Equal(t, n, l.SetWithNotification(1, v, n))
+	mock.AssertExpectationsForObjects(t, l, n, v)
+}

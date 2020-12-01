7790067a6a85e16c1c839180e82f3d01794a50b4
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-12-01 10:55:25 +0100

    [ecore][go] test EObjectImpl EInvokeFromID method

---
+++ ecore/eobject_ext_test.go
@@ -0,0 +1,51 @@
+package ecore
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEObjectEInvokeFromID(t *testing.T) {
+	o := NewEObjectImpl()
+	assert.Panics(t, func() { o.EInvokeFromID(-1, nil) })
+	assert.NotPanics(t, func() { o.EInvokeFromID(EOBJECT__EALL_CONTENTS, NewImmutableEList([]interface{}{})) })
+	assert.NotPanics(t, func() { o.EInvokeFromID(EOBJECT__ECLASS, NewImmutableEList([]interface{}{})) })
+	assert.NotPanics(t, func() { o.EInvokeFromID(EOBJECT__ECONTAINER, NewImmutableEList([]interface{}{})) })
+	assert.NotPanics(t, func() { o.EInvokeFromID(EOBJECT__ECONTAINING_FEATURE, NewImmutableEList([]interface{}{})) })
+	assert.NotPanics(t, func() { o.EInvokeFromID(EOBJECT__ECONTAINMENT_FEATURE, NewImmutableEList([]interface{}{})) })
+	assert.NotPanics(t, func() { o.EInvokeFromID(EOBJECT__ECONTENTS, NewImmutableEList([]interface{}{})) })
+	assert.NotPanics(t, func() { o.EInvokeFromID(EOBJECT__ECROSS_REFERENCES, NewImmutableEList([]interface{}{})) })
+	assert.Panics(t, func() {
+		mockFeature := new(MockEStructuralFeature)
+		mockFeature.On("GetName").Return("name")
+		o.EInvokeFromID(EOBJECT__EGET_ESTRUCTURALFEATURE, NewImmutableEList([]interface{}{mockFeature}))
+	})
+	assert.Panics(t, func() {
+		mockFeature := new(MockEStructuralFeature)
+		mockFeature.On("GetName").Return("name")
+		o.EInvokeFromID(EOBJECT__EGET_ESTRUCTURALFEATURE_EBOOLEAN, NewImmutableEList([]interface{}{mockFeature, true}))
+	})
+	assert.Panics(t, func() {
+		mockOperation := new(MockEOperation)
+		mockOperation.On("GetName").Return("name")
+		o.EInvokeFromID(EOBJECT__EINVOKE_EOPERATION_EELIST, NewImmutableEList([]interface{}{mockOperation, NewEmptyBasicEList()}))
+	})
+	assert.NotPanics(t, func() { o.EInvokeFromID(EOBJECT__EIS_PROXY, NewImmutableEList([]interface{}{})) })
+	assert.Panics(t, func() {
+		mockFeature := new(MockEStructuralFeature)
+		mockFeature.On("GetName").Return("name")
+		o.EInvokeFromID(EOBJECT__EIS_SET_ESTRUCTURALFEATURE, NewImmutableEList([]interface{}{mockFeature}))
+	})
+	assert.NotPanics(t, func() { o.EInvokeFromID(EOBJECT__ERESOURCE, NewImmutableEList([]interface{}{})) })
+	assert.Panics(t, func() {
+		mockFeature := new(MockEStructuralFeature)
+		mockFeature.On("GetName").Return("name")
+		o.EInvokeFromID(EOBJECT__ESET_ESTRUCTURALFEATURE_EJAVAOBJECT, NewImmutableEList([]interface{}{mockFeature, interface{}(nil)}))
+	})
+	assert.Panics(t, func() {
+		mockFeature := new(MockEStructuralFeature)
+		mockFeature.On("GetName").Return("name")
+		o.EInvokeFromID(EOBJECT__EUNSET_ESTRUCTURALFEATURE, NewImmutableEList([]interface{}{mockFeature}))
+	})
+}

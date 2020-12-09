dd49c5dbe9a9bcdc8b458ec5f21aab404e5f815e
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-12-09 12:03:53 +0100

    [ecore][go] EStructuralFeatureExt::GetDefaultValue and SetDefaultValue tests

---
+++ ecore/estructuralfeature_ext_test.go
@@ -0,0 +1,78 @@
+package ecore
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestEStructuralFeatureExtGetDefaultValue(t *testing.T) {
+	{
+		o := newEStructuralFeatureExt()
+		assert.Nil(t, o.GetDefaultValue())
+	}
+	{
+		// with a type & no literal
+		o := newEStructuralFeatureExt()
+		mockType := &MockEDataType{}
+		o.SetEType(mockType)
+
+		mockDefaultValue := &MockEObject{}
+		mockType.On("EIsProxy").Return(false).Once()
+		mockType.On("GetDefaultValue").Return(mockDefaultValue).Once()
+		assert.Equal(t, mockDefaultValue, o.GetDefaultValue())
+		mock.AssertExpectationsForObjects(t, mockType, mockDefaultValue)
+	}
+	{
+		// with a type & no literal & many
+		o := newEStructuralFeatureExt()
+		mockType := &MockEDataType{}
+		o.SetEType(mockType)
+		o.SetUpperBound(UNBOUNDED_MULTIPLICITY)
+
+		mockType.On("EIsProxy").Return(false).Once()
+		assert.Nil(t, o.GetDefaultValue())
+		mock.AssertExpectationsForObjects(t, mockType)
+	}
+	{
+		// with a type & literal
+		o := newEStructuralFeatureExt()
+		mockType := &MockEDataType{}
+		mockDefaultValue := &MockEObject{}
+		mockPackage := &MockEPackage{}
+		mockFactory := &MockEFactory{}
+		o.SetEType(mockType)
+		o.SetDefaultValueLiteral("defaultLiteralValue")
+		mockType.On("EIsProxy").Return(false).Once()
+		mockType.On("GetEPackage").Return(mockPackage).Once()
+		mockType.On("IsSerializable").Return(true).Once()
+		mockPackage.On("GetEFactoryInstance").Return(mockFactory).Once()
+		mockFactory.On("CreateFromString", mockType, "defaultLiteralValue").Return(mockDefaultValue).Once()
+		assert.Equal(t, mockDefaultValue, o.GetDefaultValue())
+		mock.AssertExpectationsForObjects(t, mockType, mockDefaultValue, mockPackage, mockFactory)
+	}
+}
+
+func TestEStructuralFeatureExtSetDefaultValue(t *testing.T) {
+	{
+		o := newEStructuralFeatureExt()
+		assert.Panics(t, func() {
+			o.SetDefaultValue(nil)
+		})
+	}
+	{
+		o := newEStructuralFeatureExt()
+		mockType := &MockEDataType{}
+		mockDefaultValue := &MockEObject{}
+		mockPackage := &MockEPackage{}
+		mockFactory := &MockEFactory{}
+		o.SetEType(mockType)
+		mockType.On("EIsProxy").Return(false).Once()
+		mockType.On("GetEPackage").Return(mockPackage).Once()
+		mockPackage.On("GetEFactoryInstance").Return(mockFactory).Once()
+		mockFactory.On("ConvertToString", mockType, mockDefaultValue).Return("defaultValueLiteral").Once()
+		o.SetDefaultValue(mockDefaultValue)
+		mock.AssertExpectationsForObjects(t, mockType, mockDefaultValue, mockPackage, mockFactory)
+	}
+}

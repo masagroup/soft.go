25916644ff6d3ce0fdec9f1473a804e95704169d
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-12-09 15:27:52 +0100

    [ecore][go] define EDataTypeExt class and SetDefaultValue method

---
+++ ecore/edatatype_ext.go
@@ -0,0 +1,36 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2019 MASA Group
+//
+// *****************************************************************************
+
+package ecore
+
+type EDataTypeInternal interface {
+	EDataType
+
+	SetDefaultValue(defaultValue interface{})
+}
+
+type eDataTypeExt struct {
+	*eDataTypeImpl
+	defaultValue interface{}
+}
+
+func newEDataTypeExt() *eDataTypeExt {
+	eDataType := new(eDataTypeExt)
+	eDataType.eDataTypeImpl = newEDataTypeImpl()
+	eDataType.interfaces = eDataType
+	return eDataType
+}
+
+func (eDataType *eDataTypeExt) SetDefaultValue(newDefaultValue interface{}) {
+	oldDefaultValue := eDataType.defaultValue
+	eDataType.defaultValue = newDefaultValue
+	if eDataType.ENotificationRequired() {
+		eDataType.ENotify(NewNotificationByFeatureID(eDataType.AsEObject(), SET, EDATA_TYPE__DEFAULT_VALUE, oldDefaultValue, newDefaultValue, NO_INDEX))
+	}
+}

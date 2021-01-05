844c466811acaf41176c6d14fe892198a9036616
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2021-01-05 10:29:51 +0100

    [ecore][go] AbstractNotifier tests

--- ecore/abstractenotifier_test.go
+++ ecore/abstractenotifier_test.go
@@ -2,24 +2,32 @@ package ecore
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestNotifierAdapter(t *testing.T) {
-	// notifier := NewBasicNotifier()
-	// mockEAdapter := new(MockEAdapter)
-	// mockEAdapter.On("SetTarget", notifier).Once()
-	// notifier.EAdapters().Add(mockEAdapter)
+func TestAbstractNotifier(t *testing.T) {
+	notifier := &AbstractENotifier{}
+	notifier.SetInterfaces(notifier)
+	assert.Equal(t, notifier, notifier.GetInterfaces())
+	assert.Equal(t, notifier, notifier.AsENotifier())
+	assert.Equal(t, notifier, notifier.AsENotifierInternal())
+	assert.Nil(t, notifier.EBasicAdapters())
+	assert.False(t, notifier.EBasicHasAdapters())
+	assert.NotNil(t, notifier.EAdapters())
+	assert.True(t, notifier.EAdapters().Empty())
+	assert.False(t, notifier.EDeliver())
+	assert.Panics(t, func() {
+		notifier.ESetDeliver(false)
+	})
+}
 
-	// mockEAdapter.On("NotifyChanged", mock.MatchedBy(func(n ENotification) bool {
-	// 	return n.GetNotifier() == notifier &&
-	// 		n.GetFeatureID() == -1 &&
-	// 		n.GetFeature() == nil &&
-	// 		n.GetNewValue() == nil &&
-	// 		n.GetOldValue() == mockEAdapter &&
-	// 		n.GetEventType() == REMOVING_ADAPTER &&
-	// 		n.GetPosition() == 0
-	// })).Once()
-	// mockEAdapter.On("UnSetTarget", notifier).Once()
-	// notifier.EAdapters().Remove(mockEAdapter)
-	// mockEAdapter.AssertExpectations(t)
+func TestNotifierNotification(t *testing.T) {
+	notifier := &AbstractENotifier{}
+	notifier.SetInterfaces(notifier)
+	notification := &notifierNotification{notifier: notifier}
+
+	assert.Equal(t, notifier, notification.GetNotifier())
+	assert.Equal(t, nil, notification.GetFeature())
+	assert.Equal(t, -1, notification.GetFeatureID())
 }

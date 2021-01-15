88f577b250593101fce95a0f7baf5d441019d5fb
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2021-01-15 16:31:13 +0100

    [library] add library xml serialization tests

--- library/serialization_test.go
+++ library/serialization_test.go
@@ -66,38 +66,12 @@ func TestSerializationLoadComplexXML(t *testing.T) {
 	assert.True(t, resource.GetWarnings().Empty(), diagnosticError(resource.GetWarnings()))
 }
 
-func TestSerializationLoadSaveSimpleXML(t *testing.T) {
-	ecore.GetPackageRegistry().RegisterPackage(GetPackage())
-
-	fileURI := &url.URL{Path: "testdata/library.simple.xml"}
-	resourceFactory := ecore.GetResourceFactoryRegistry().GetFactory(fileURI)
-	resource := resourceFactory.CreateResource(fileURI)
-	resource.Load()
-
-	resource.SetURI(&url.URL{Path: "testdata/library.simple.output.xml"})
-	resource.Save()
-}
-
-func TestSerializationLoadSaveXML(t *testing.T) {
-	ecore.GetPackageRegistry().RegisterPackage(GetPackage())
-
-	fileURI := &url.URL{Path: "testdata/library.xml"}
-	resourceFactory := ecore.GetResourceFactoryRegistry().GetFactory(fileURI)
-	resource := resourceFactory.CreateResource(fileURI)
-	resource.Load()
-
-	var strbuff strings.Builder
-	resource.SaveWithWriter(&strbuff)
-
-	bytes, err := ioutil.ReadFile("testdata/library.xml")
-	assert.Nil(t, err)
-	assert.Equal(t, strings.ReplaceAll(string(bytes), "\r\n", "\n"), strings.ReplaceAll(strbuff.String(), "\r\n", "\n"))
-}
-
-func TestSerializationSaveXmlFile(t *testing.T) {
+func TestSerializationSaveSimpleXml(t *testing.T) {
 	// create a library model with a single employee
 	root := GetFactory().CreateDocumentRoot()
 	library := GetFactory().CreateLibrary()
+	library.SetName("My Library")
+	library.SetAddress("My Library Adress")
 	root.SetLibrary(library)
 
 	employee := GetFactory().CreateEmployee()
@@ -108,24 +82,72 @@ func TestSerializationSaveXmlFile(t *testing.T) {
 
 	// save library model with a resource
 	ecore.GetPackageRegistry().RegisterPackage(GetPackage())
-	fileURI := &url.URL{Path: "testdata/simple.output.xml"}
+	fileURI := &url.URL{Path: "testdata/dynamic.simple.output.xml"}
 	resourceFactory := ecore.GetResourceFactoryRegistry().GetFactory(fileURI)
 	resource := resourceFactory.CreateResource(fileURI)
 	resource.GetContents().Add(root)
 	resource.Save()
 
-	bytesInput, errInput := ioutil.ReadFile("testdata/simple.input.xml")
+	bytesInput, errInput := ioutil.ReadFile("testdata/dynamic.simple.result.xml")
 	assert.Nil(t, errInput)
-	bytesOutput, errOutput := ioutil.ReadFile("testdata/simple.output.xml")
+	bytesOutput, errOutput := ioutil.ReadFile("testdata/dynamic.simple.output.xml")
 	assert.Nil(t, errOutput)
 	assert.Equal(t, strings.ReplaceAll(string(bytesInput), "\r\n", "\n"), strings.ReplaceAll(string(bytesOutput), "\r\n", "\n"))
 
 }
 
+func TestSerializationLoadSaveSimpleXML(t *testing.T) {
+	ecore.GetPackageRegistry().RegisterPackage(GetPackage())
+
+	fileURI := &url.URL{Path: "testdata/library.simple.default.xml"}
+	resourceFactory := ecore.GetResourceFactoryRegistry().GetFactory(fileURI)
+	resource := resourceFactory.CreateResource(fileURI)
+	resource.Load()
+
+	var strbuff strings.Builder
+	resource.SaveWithWriter(&strbuff)
+
+	bytes, err := ioutil.ReadFile("testdata/library.simple.default.xml")
+	assert.Nil(t, err)
+	assert.Equal(t, strings.ReplaceAll(string(bytes), "\r\n", "\n"), strings.ReplaceAll(strbuff.String(), "\r\n", "\n"))
+}
+
+func TestSerializationLoadSavePrefixXML(t *testing.T) {
+	ecore.GetPackageRegistry().RegisterPackage(GetPackage())
+
+	fileURI := &url.URL{Path: "testdata/library.simple.prefix.xml"}
+	resourceFactory := ecore.GetResourceFactoryRegistry().GetFactory(fileURI)
+	resource := resourceFactory.CreateResource(fileURI)
+	resource.Load()
+
+	var strbuff strings.Builder
+	resource.SaveWithWriter(&strbuff)
+
+	bytes, err := ioutil.ReadFile("testdata/library.simple.prefix.xml")
+	assert.Nil(t, err)
+	assert.Equal(t, strings.ReplaceAll(string(bytes), "\r\n", "\n"), strings.ReplaceAll(strbuff.String(), "\r\n", "\n"))
+}
+
+func TestSerializationLoadSaveComplexXML(t *testing.T) {
+	ecore.GetPackageRegistry().RegisterPackage(GetPackage())
+
+	fileURI := &url.URL{Path: "testdata/library.complex.xml"}
+	resourceFactory := ecore.GetResourceFactoryRegistry().GetFactory(fileURI)
+	resource := resourceFactory.CreateResource(fileURI)
+	resource.Load()
+
+	var strbuff strings.Builder
+	resource.SaveWithWriter(&strbuff)
+
+	bytes, err := ioutil.ReadFile("testdata/library.complex.xml")
+	assert.Nil(t, err)
+	assert.Equal(t, strings.ReplaceAll(string(bytes), "\r\n", "\n"), strings.ReplaceAll(strbuff.String(), "\r\n", "\n"))
+}
+
 func TestDeepOperations(t *testing.T) {
 	ecore.GetPackageRegistry().RegisterPackage(GetPackage())
 
-	fileURI := &url.URL{Path: "testdata/library.xml"}
+	fileURI := &url.URL{Path: "testdata/library.complex.xml"}
 	resourceFactory := ecore.GetResourceFactoryRegistry().GetFactory(fileURI)
 	resource := resourceFactory.CreateResource(fileURI)
 	resource.Load()

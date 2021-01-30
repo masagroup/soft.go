d430cae7ba4394154415607662b0ecb376f48a1c
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2021-01-30 10:13:50 +0100

    [ecore][go] define methods in EPackageExt to simplify class/attributes/references/operations/datatypes initialization

--- ecore/epackage_ext.go
+++ ecore/epackage_ext.go
@@ -67,3 +67,80 @@ func (pack *EPackageExt) CreateResource() EResource {
 	}
 	return resource
 }
+
+func (pack *EPackageExt) InitEClass(eClass EClass, name string, instanceTypeName string, isAbstract bool, isInterface bool) {
+	eClass.SetName(name)
+	eClass.SetAbstract(isAbstract)
+	eClass.SetInterface(isInterface)
+	eClass.SetInstanceTypeName(instanceTypeName)
+}
+
+func (pack *EPackageExt) initEStructuralFeature(aFeature EStructuralFeature, aClassifier EClassifier, name, defaultValue string, lowerBound, upperBound int, isTransient, isVolatile, isChangeable, isUnSettable, isUnique, isDerived, isOrdered bool) {
+	aFeature.SetName(name)
+	aFeature.SetEType(aClassifier)
+	aFeature.SetDefaultValueLiteral(defaultValue)
+	aFeature.SetLowerBound(lowerBound)
+	aFeature.SetUpperBound(upperBound)
+	aFeature.SetTransient(isTransient)
+	aFeature.SetVolatile(isVolatile)
+	aFeature.SetChangeable(isChangeable)
+	aFeature.SetUnsettable(isUnSettable)
+	aFeature.SetUnique(isUnique)
+	aFeature.SetDerived(isDerived)
+	aFeature.SetOrdered(isOrdered)
+}
+
+func (pack *EPackageExt) InitEAttribute(aAttribute EAttribute, aType EClassifier, name, defaultValue string, lowerBound, upperBound int, isTransient, isVolatile, isChangeable, isUnSettable, isUnique, isDerived, isOrdered, isID bool) {
+	pack.initEStructuralFeature(aAttribute, aType, name, defaultValue, lowerBound, upperBound, isTransient, isVolatile, isChangeable, isUnSettable, isUnique, isDerived, isOrdered)
+	aAttribute.SetID(isID)
+}
+
+func (pack *EPackageExt) InitEReference(aReference EReference, aType EClassifier, aOtherEnd EReference, name, defaultValue string, lowerBound, upperBound int, isTransient, isVolatile, isChangeable, isContainment, isResolveProxies, isUnSettable, isUnique, isDerived, isOrdered bool) {
+	pack.initEStructuralFeature(aReference, aType, name, defaultValue, lowerBound, upperBound, isTransient, isVolatile, isChangeable, isUnSettable, isUnique, isDerived, isOrdered)
+	aReference.SetContainment(isContainment)
+	aReference.SetResolveProxies(isResolveProxies)
+	aReference.SetEOpposite(aOtherEnd)
+}
+
+func (pack *EPackageExt) InitEOperation(aOperation EOperation, aType EClassifier, name string, lowerBound, upperBound int, isUnique, isOrdered bool) {
+	aOperation.SetName(name)
+	aOperation.SetEType(aType)
+	aOperation.SetLowerBound(lowerBound)
+	aOperation.SetUpperBound(upperBound)
+	aOperation.SetUnique(isUnique)
+	aOperation.SetOrdered(isOrdered)
+}
+
+func (pack *EPackageExt) AddEParameter(aOperation EOperation, aType EClassifier, name string, lowerBound, upperBound int, isUnique, isOrdered bool) {
+	parameter := GetFactory().CreateEParameterFromContainer(aOperation)
+	parameter.SetName(name)
+	parameter.SetEType(aType)
+	parameter.SetLowerBound(lowerBound)
+	parameter.SetUpperBound(upperBound)
+	parameter.SetUnique(isUnique)
+	parameter.SetOrdered(isOrdered)
+}
+
+func (pack *EPackageExt) initEClassifier(aClassifier EClassifier, name, instanceTypeName string) {
+	aClassifier.SetName(name)
+	aClassifier.SetInstanceTypeName(instanceTypeName)
+}
+
+func (pack *EPackageExt) InitEDataType(aDataType EDataType, name, instanceTypeName, defaultValue string, isSerializable bool) {
+	pack.initEClassifier(aDataType, name, instanceTypeName)
+	aDataType.SetSerializable(isSerializable)
+	if len(defaultValue) > 0 {
+		aDataType.(EDataTypeInternal).SetDefaultValue(pack.GetEFactoryInstance().CreateFromString(aDataType, defaultValue))
+	}
+}
+
+func (pack *EPackageExt) InitEEnum(aEnum EEnum, name, instanceTypeName string) {
+	pack.initEClassifier(aEnum, name, instanceTypeName)
+}
+
+func (pack *EPackageExt) AddEEnumLiteral(aEnum EEnum, name, literal string, value int) {
+	enumLiteral := GetFactory().CreateEEnumLiteralFromContainer(aEnum)
+	enumLiteral.SetName(name)
+	enumLiteral.SetLiteral(literal)
+	enumLiteral.SetValue(value)
+}

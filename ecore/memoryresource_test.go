73181b0a891c4fa7f2f08632ccb218bbbca3a825
JDY <jocelyn@barry-dumay.com>
2021-05-11 10:11:49 +0200

    [ecore][go] add memory resource factory in resource factory registry

--- ecore/memoryresource_test.go
+++ ecore/memoryresource_test.go
@@ -1,1 +1,42 @@
 package ecore
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMemoryResourceIsLoaded(t *testing.T) {
+	r := newMemoryResourceImpl()
+	assert.True(t, r.IsLoaded())
+}
+
+func TestMemoryResourceLoad(t *testing.T) {
+	r := newMemoryResourceImpl()
+	assert.Panics(t, func() { r.Load() })
+}
+
+func TestMemoryResourceLoadWithOptions(t *testing.T) {
+	r := newMemoryResourceImpl()
+	assert.Panics(t, func() { r.LoadWithOptions(nil) })
+}
+
+func TestMemoryResourceLoadWithWriter(t *testing.T) {
+	r := newMemoryResourceImpl()
+	assert.Panics(t, func() { r.LoadWithReader(nil, nil) })
+}
+
+func TestMemoryResourceSave(t *testing.T) {
+	r := newMemoryResourceImpl()
+	assert.Panics(t, func() { r.Save() })
+}
+
+func TestMemoryResourceSaveWithOptions(t *testing.T) {
+	r := newMemoryResourceImpl()
+	assert.Panics(t, func() { r.SaveWithOptions(nil) })
+}
+
+func TestMemoryResourceSaveWithWriter(t *testing.T) {
+	r := newMemoryResourceImpl()
+	assert.Panics(t, func() { r.SaveWithWriter(nil, nil) })
+}

2a36312fa152164f7ff43a4be073866c508c7fba
JDY <jocelyn@barry-dumay.com>
2021-06-21 12:25:59 +0200

    [ecore][go] add MemeoryURIHandler tests

---
+++ ecore/memoryurihandler_test.go
@@ -0,0 +1,27 @@
+package ecore
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMemoryURIHandler_CanHandle(t *testing.T) {
+	m := &MemoryURIHandler{}
+	assert.True(t, m.CanHandle(&URI{Scheme: "memory"}))
+	assert.False(t, m.CanHandle(&URI{Scheme: "file"}))
+}
+
+func TestMemoryURIHandler_CreateReader(t *testing.T) {
+	m := &MemoryURIHandler{}
+	r, err := m.CreateReader(nil)
+	assert.Nil(t, r)
+	assert.Nil(t, err)
+}
+
+func TestMemoryURIHandler_CreateWriter(t *testing.T) {
+	m := &MemoryURIHandler{}
+	r, err := m.CreateWriter(nil)
+	assert.Nil(t, r)
+	assert.Nil(t, err)
+}

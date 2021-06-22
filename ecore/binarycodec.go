7aa0b0782703f160c842285ced02dfeb1fe64e4f
JDY <jocelyn@barry-dumay.com>
2021-06-22 09:53:55 +0200

    [ecore][go] binary encoder implementation

--- ecore/binarycodec.go
+++ ecore/binarycodec.go
@@ -9,7 +9,9 @@
 
 package ecore
 
-import "io"
+import (
+	"io"
+)
 
 type BinaryCodec struct {
 }
@@ -19,4 +21,90 @@ func (bc *BinaryCodec) NewEncoder(resource EResource, w io.Writer, options map[s
 }
 func (bc *BinaryCodec) NewDecoder(resource EResource, r io.Reader, options map[string]interface{}) EResourceDecoder {
 	return NewBinaryDecoder(resource, r, options)
+}
+
+const (
+	object_container       = iota
+	object_container_proxy = iota
+
+	object       = iota
+	object_proxy = iota
+
+	object_list       = iota
+	object_list_proxy = iota
+
+	object_containment       = iota
+	object_containment_proxy = iota
+
+	object_containment_list       = iota
+	object_containment_list_proxy = iota
+
+	data      = iota
+	data_list = iota
+	enum      = iota
+	date      = iota
+	primitive = iota
+)
+
+func getBinaryCodecFeatureKind(eFeature EStructuralFeature) int {
+	if eReference, _ := eFeature.(EReference); eReference != nil {
+		if eReference.IsContainment() {
+			if eReference.IsResolveProxies() {
+				if eReference.IsMany() {
+					return object_containment_list_proxy
+				} else {
+					return object_containment_proxy
+				}
+			} else {
+				if eReference.IsMany() {
+					return object_containment_list
+				} else {
+					return object_containment
+				}
+			}
+		} else if eReference.IsContainer() {
+			if eReference.IsResolveProxies() {
+				return object_container_proxy
+			} else {
+				return object_container
+			}
+		} else if eReference.IsResolveProxies() {
+			if eReference.IsMany() {
+				return object_list_proxy
+			} else {
+				return object_list
+			}
+		} else {
+			if eReference.IsMany() {
+				return object_list
+			} else {
+				return object
+			}
+		}
+	} else if eAttribute, _ := eFeature.(EAttribute); eAttribute != nil {
+		if eAttribute.IsMany() {
+			return data_list
+		} else {
+			eDataType := eAttribute.GetEAttributeType()
+			if eEnum := eDataType.(EEnum); eEnum != nil {
+				return enum
+			}
+			instanceTypeName := eDataType.GetInstanceTypeName()
+			if instanceTypeName == "float64" ||
+				instanceTypeName == "float32" ||
+				instanceTypeName == "int" ||
+				instanceTypeName == "int64" ||
+				instanceTypeName == "int32" ||
+				instanceTypeName == "int16" ||
+				instanceTypeName == "bool" ||
+				instanceTypeName == "string" {
+				return primitive
+			}
+			if instanceTypeName == "*time.Time" {
+				return date
+			}
+			return data
+		}
+	}
+	return -1
 }
--- ecore/binaryencoder.go
+++ ecore/binaryencoder.go
@@ -11,33 +11,79 @@ package ecore
 
 import (
 	"io"
+	"time"
 
 	"github.com/ugorji/go/codec"
 )
 
+type checkType int
+
 const (
-	checkNothing = iota
+	checkNothing checkType = iota
 	checkDirectResource
 	checkResource
-	chechContainer
+	checkContainer
 )
 
+const encoderVersion = 0
+
+type binaryEncoderPackageData struct {
+	id        int
+	classData []*binaryEncoderClassData
+}
+
+type binaryEncoderClassData struct {
+	packageID   int
+	id          int
+	featureData []*binaryEncoderFeatureData
+}
+
+type binaryEncoderFeatureData struct {
+	name        string
+	isTransient bool
+	featureKind int
+	factory     EFactory
+	dataType    EDataType
+}
+
 type BinaryEncoder struct {
-	w        io.Writer
-	resource EResource
-	baseURI  *URI
-	encoder  *codec.Encoder
-	version  int
+	w              io.Writer
+	resource       EResource
+	encoder        *codec.Encoder
+	baseURI        *URI
+	version        int
+	objectToID     map[EObject]int
+	classDataMap   map[EClass]*binaryEncoderClassData
+	packageDataMap map[EPackage]*binaryEncoderPackageData
+	uriToIDMap     map[string]int
 }
 
 func NewBinaryEncoder(resource EResource, w io.Writer, options map[string]interface{}) *BinaryEncoder {
-	return &BinaryEncoder{w: w, resource: resource}
+	return NewBinaryEncoderWithVersion(resource, w, options, encoderVersion)
+}
+
+func NewBinaryEncoderWithVersion(resource EResource, w io.Writer, options map[string]interface{}, version int) *BinaryEncoder {
+	mh := &codec.MsgpackHandle{}
+	e := &BinaryEncoder{
+		w:              w,
+		resource:       resource,
+		encoder:        codec.NewEncoder(w, mh),
+		version:        version,
+		objectToID:     map[EObject]int{},
+		classDataMap:   map[EClass]*binaryEncoderClassData{},
+		packageDataMap: map[EPackage]*binaryEncoderPackageData{},
+	}
+	if uri := resource.GetURI(); uri != nil && uri.IsAbsolute() {
+		e.baseURI = uri
+	}
+	e.encodeVersion()
+	e.encodeSignature()
+	return e
 }
 
 func (be *BinaryEncoder) Encode() {
-	be.encoder = codec.NewEncoder(be.w, &codec.MsgpackHandle{})
-	be.encodeSignature()
-	be.encodeVersion()
+	contents := be.resource.GetContents()
+	be.encodeObjects(contents, checkContainer)
 }
 
 func (be *BinaryEncoder) EncodeObject(object EObject) error {
@@ -53,4 +99,230 @@ func (be *BinaryEncoder) encodeSignature() {
 
 func (be *BinaryEncoder) encodeVersion() {
 	be.encoder.Encode(be.version)
+}
+
+func (be *BinaryEncoder) encodeObjects(objects EList, check checkType) {
+	be.encoder.Encode(objects.Size())
+	for it := objects.Iterator(); it.HasNext(); {
+		switch eObject := it.Next().(type) {
+		case nil:
+			be.encodeObject(nil, check)
+		case EObject:
+			be.encodeObject(eObject, check)
+		}
+	}
+}
+
+func (be *BinaryEncoder) encodeObject(eObject EObject, check checkType) {
+	if eObject == nil {
+		be.encoder.Encode(nil)
+	} else if id, isID := be.objectToID[eObject]; isID {
+		be.encoder.Encode(id)
+	} else {
+		// object id
+		var objectID interface{}
+		if objectIDManager := be.resource.GetObjectIDManager(); objectIDManager != nil {
+			objectID = objectIDManager.GetID(eObject)
+		} else {
+			id := len(be.objectToID)
+			be.objectToID[eObject] = id
+			objectID = id
+		}
+		be.encoder.Encode(objectID)
+		// object class
+		eClass := eObject.EClass()
+		eClassData := be.encodeClass(eClass)
+
+		saveFeatureValues := true
+		eObjectInternal, _ := eObject.(EObjectInternal)
+		if eObjectInternal == nil {
+			return
+		}
+		// object uri if reference or proxy
+		switch check {
+		case checkDirectResource:
+			if eResource := eObjectInternal.EInternalResource(); eResource != nil {
+				be.encoder.Encode(0)
+				be.encodeURIWithFragment(eResource.GetURI(), eResource.GetURIFragment(eObjectInternal))
+				saveFeatureValues = false
+			} else if eObjectInternal.EIsProxy() {
+				be.encoder.Encode(0)
+				be.encodeURI(eObjectInternal.EProxyURI())
+				saveFeatureValues = false
+			}
+		case checkResource:
+			if eResource := eObjectInternal.EResource(); eResource != nil && eResource != be.resource {
+				be.encoder.Encode(0)
+				be.encodeURIWithFragment(eResource.GetURI(), eResource.GetURIFragment(eObjectInternal))
+				saveFeatureValues = false
+			} else if eObjectInternal.EIsProxy() {
+				be.encoder.Encode(0)
+				be.encodeURI(eObjectInternal.EProxyURI())
+				saveFeatureValues = false
+			}
+		case checkNothing:
+		case checkContainer:
+		}
+		// object feature values
+		for featureID, featureData := range eClassData.featureData {
+			if saveFeatureValues && !featureData.isTransient && (check == checkContainer || featureData.featureKind != object_container_proxy) {
+				be.encodeFeatureValue(eObjectInternal, featureID, featureData)
+			}
+		}
+		be.encoder.Encode(1)
+	}
+}
+
+func (be *BinaryEncoder) encodeFeatureValue(eObject EObjectInternal, featureID int, featureData *binaryEncoderFeatureData) {
+	if eObject.EIsSetFromID(featureID) {
+		be.encoder.Encode(featureID)
+		if len(featureData.name) > 0 {
+			be.encoder.Encode(featureData.name)
+			featureData.name = ""
+		}
+		value := eObject.EGetFromID(featureID, false)
+		switch featureData.featureKind {
+		case object:
+			fallthrough
+		case object_containment:
+			be.encodeObject(value.(EObject), checkNothing)
+		case object_container_proxy:
+			be.encodeObject(value.(EObject), checkResource)
+		case object_containment_proxy:
+			be.encodeObject(value.(EObject), checkDirectResource)
+		case object_proxy:
+			be.encodeObject(value.(EObject), checkResource)
+		case object_list:
+			fallthrough
+		case object_containment_list:
+			be.encodeObjects(value.(EList), checkNothing)
+		case object_containment_list_proxy:
+			be.encodeObjects(value.(EList), checkDirectResource)
+		case object_list_proxy:
+			be.encodeObjects(value.(EList), checkResource)
+		case data:
+			valueStr := featureData.factory.ConvertToString(featureData.dataType, value)
+			be.encoder.Encode(valueStr)
+		case data_list:
+			valuesStr := []string{}
+			for it := value.(EList).Iterator(); it.HasNext(); {
+				value := it.Next()
+				valueStr := featureData.factory.ConvertToString(featureData.dataType, value)
+				valuesStr = append(valuesStr, valueStr)
+			}
+			be.encoder.Encode(valuesStr)
+		case enum:
+			be.encoder.Encode(value)
+		case date:
+			t := value.(*time.Time)
+			be.encoder.Encode(t.Unix())
+		case primitive:
+			be.encoder.Encode(value)
+		}
+	}
+}
+
+func (be *BinaryEncoder) encodeClass(eClass EClass) *binaryEncoderClassData {
+	eClassData, _ := be.classDataMap[eClass]
+	if eClassData != nil {
+		be.encoder.Encode(eClassData.packageID)
+		be.encoder.Encode(eClassData.id)
+	} else {
+		eClassData := be.newClassData(eClass)
+		be.encoder.Encode(eClassData.id)
+		be.encoder.Encode(eClass.GetName())
+	}
+	return eClassData
+}
+
+func (be *BinaryEncoder) encodePackage(ePackage EPackage) *binaryEncoderPackageData {
+	ePackageData, _ := be.packageDataMap[ePackage]
+	if ePackageData != nil {
+		be.encoder.Encode(ePackageData.id)
+	} else {
+		ePackageData := be.newPackageData(ePackage)
+		be.encoder.Encode(ePackageData.id)
+		be.encoder.Encode(ePackage.GetNsURI())
+		be.encodeURI(GetURI(ePackage))
+		be.packageDataMap[ePackage] = ePackageData
+	}
+	return ePackageData
+}
+
+func (be *BinaryEncoder) encodeURI(uri *URI) {
+	if uri == nil {
+		be.encoder.Encode(nil)
+	} else {
+		be.encodeURIWithFragment(uri.TrimFragment(), uri.Fragment)
+	}
+}
+
+func (be *BinaryEncoder) encodeURIWithFragment(uri *URI, fragment string) {
+	if uri == nil {
+		be.encoder.Encode(nil)
+	} else {
+		uriPath := uri.String()
+		if id, isID := be.uriToIDMap[uriPath]; isID {
+			be.encoder.Encode(id)
+		} else {
+			id := len(be.uriToIDMap)
+			be.uriToIDMap[uriPath] = id
+			be.encoder.Encode(id)
+			be.encoder.Encode(be.relativizeURI(uri))
+		}
+		be.encoder.Encode(fragment)
+	}
+}
+
+func (be *BinaryEncoder) relativizeURI(uri *URI) *URI {
+	if be.baseURI != nil {
+		return be.baseURI.Relativize(uri)
+	}
+	return uri
+}
+
+func (be *BinaryEncoder) newPackageData(ePackage EPackage) *binaryEncoderPackageData {
+	return &binaryEncoderPackageData{
+		id:        len(be.packageDataMap),
+		classData: make([]*binaryEncoderClassData, ePackage.GetEClassifiers().Size()),
+	}
+}
+
+func (be *BinaryEncoder) newClassID(ePackageData *binaryEncoderPackageData) int {
+	for i, c := range ePackageData.classData {
+		if c == nil {
+			return i
+		}
+	}
+	return -1
+}
+
+func (be *BinaryEncoder) newClassData(eClass EClass) *binaryEncoderClassData {
+	ePackageData := be.encodePackage(eClass.GetEPackage())
+	eClassData := &binaryEncoderClassData{
+		packageID:   ePackageData.id,
+		id:          be.newClassID(ePackageData),
+		featureData: []*binaryEncoderFeatureData{},
+	}
+	for it := eClass.GetEStructuralFeatures().Iterator(); it.HasNext(); {
+		eFeature := it.Next().(EStructuralFeature)
+		eClassData.featureData = append(eClassData.featureData, be.newFeatureData(eFeature))
+	}
+	return eClassData
+}
+
+func (be *BinaryEncoder) newFeatureData(eFeature EStructuralFeature) *binaryEncoderFeatureData {
+	eFeatureData := &binaryEncoderFeatureData{
+		name:        eFeature.GetName(),
+		featureKind: getBinaryCodecFeatureKind(eFeature),
+	}
+	if eReference, _ := eFeature.(EReference); eReference != nil {
+		eFeatureData.isTransient = eReference.IsTransient() || (eReference.IsContainer() && !eReference.IsResolveProxies())
+	} else if eAttribute, _ := eFeature.(EAttribute); eAttribute != nil {
+		eDataType := eAttribute.GetEAttributeType()
+		eFeatureData.isTransient = eReference.IsTransient()
+		eFeatureData.dataType = eDataType
+		eFeatureData.factory = eDataType.GetEPackage().GetEFactoryInstance()
+	}
+	return eFeatureData
 }

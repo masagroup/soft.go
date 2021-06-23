4e188a00c65b8072507e87c9b2dcdbe02883857c
JDY <jocelyn@barry-dumay.com>
2021-06-23 15:22:49 +0200

    [ecore][go] add binary decoder test with library.complex.big

--- ecore/binarydecoder_test.go
+++ ecore/binarydecoder_test.go
@@ -28,3 +28,25 @@ func TestBinaryDecoder_Complex(t *testing.T) {
 	binaryDecoder.Decode()
 	require.True(t, eResource.GetErrors().Empty(), diagnosticError(eResource.GetErrors()))
 }
+
+func TestBinaryDecoder_ComplexBig(t *testing.T) {
+	// load package
+	ePackage := loadPackage("library.complex.ecore")
+	require.NotNil(t, ePackage)
+
+	//
+	uri := &URI{Path: "testdata/library.complex.big.bin"}
+	eResource := NewEResourceImpl()
+	eResource.SetURI(uri)
+	eResourceSet := NewEResourceSetImpl()
+	eResourceSet.GetResources().Add(eResource)
+	eResourceSet.GetPackageRegistry().RegisterPackage(ePackage)
+
+	// file
+	f, err := os.Open(uri.Path)
+	require.Nil(t, err)
+
+	binaryDecoder := NewBinaryDecoder(eResource, f, nil)
+	binaryDecoder.Decode()
+	require.True(t, eResource.GetErrors().Empty(), diagnosticError(eResource.GetErrors()))
+}

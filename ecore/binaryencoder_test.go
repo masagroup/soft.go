793b4de0f53af1526e80e1c1b656af817d77b726
JDY <jocelyn@barry-dumay.com>
2021-06-25 11:01:09 +0200

    [ecore][go] add BinaryEncoder/BinaryDecoder tests with object id attribute

--- ecore/binaryencoder_test.go
+++ ecore/binaryencoder_test.go
@@ -34,6 +34,40 @@ func TestBinaryEncoder_Complex(t *testing.T) {
 	assert.Equal(t, bytes, w.Bytes())
 }
 
+func TestBinaryEncoder_ComplexWithID(t *testing.T) {
+	// load package
+	ePackage := loadPackage("library.complex.ecore")
+	require.NotNil(t, ePackage)
+
+	// load resource
+	uri := &URI{Path: "testdata/library.complex.id.xml"}
+	eResource := NewEResourceImpl()
+	eResource.SetURI(uri)
+	eResource.SetObjectIDManager(NewUniqueIDManager(20))
+	eResourceSet := NewEResourceSetImpl()
+	eResourceSet.GetResources().Add(eResource)
+	eResourceSet.GetPackageRegistry().RegisterPackage(ePackage)
+	eResource.LoadWithOptions(map[string]interface{}{XML_OPTION_ID_ATTRIBUTE_NAME: "id"})
+	require.NotNil(t, eResource)
+	require.True(t, eResource.IsLoaded())
+	require.True(t, eResource.GetErrors().Empty(), diagnosticError(eResource.GetErrors()))
+	require.True(t, eResource.GetWarnings().Empty(), diagnosticError(eResource.GetWarnings()))
+
+	// set DocumentRoot uuid, because it is not defined in xml and is always regenerated
+	eDocumentRoot, _ := eResource.GetContents().Get(0).(EObject)
+	require.NotNil(t, eDocumentRoot)
+	require.Nil(t, eResource.GetObjectIDManager().SetID(eDocumentRoot, "h0Rz1FjVeBXUgaW3OzT2frUce90="))
+
+	w := &bytes.Buffer{}
+	binaryEncoder := NewBinaryEncoder(eResource, w, map[string]interface{}{BINARY_OPTION_ID_ATTRIBUTE: true})
+	binaryEncoder.Encode()
+	require.True(t, eResource.GetErrors().Empty(), diagnosticError(eResource.GetErrors()))
+
+	bytes, err := ioutil.ReadFile("testdata/library.complex.id.bin")
+	assert.Nil(t, err)
+	assert.Equal(t, bytes, w.Bytes())
+}
+
 func TestBinaryEncoder_ComplexBig(t *testing.T) {
 	// load package
 	ePackage := loadPackage("library.complex.ecore")

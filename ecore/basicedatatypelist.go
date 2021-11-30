3bb2d3722d7acb675ba52ca45200aaa42221ec3c
JDY <jocelyn@barry-dumay.com>
2021-11-30 18:37:09 +0100

    [ecore] define basicEDataTypeList struct

---
+++ ecore/basicedatatypelist.go
@@ -0,0 +1,35 @@
+package ecore
+
+type basicEDataTypeList struct {
+	BasicENotifyingList
+	owner     EObjectInternal
+	featureID int
+}
+
+func NewEDataTypeList(owner EObjectInternal, featureID int, isUnique bool) *basicEDataTypeList {
+	l := new(basicEDataTypeList)
+	l.interfaces = l
+	l.data = []interface{}{}
+	l.owner = owner
+	l.featureID = featureID
+	l.isUnique = isUnique
+	return l
+}
+
+// GetNotifier ...
+func (list *basicEDataTypeList) GetNotifier() ENotifier {
+	return list.owner
+}
+
+// GetFeature ...
+func (list *basicEDataTypeList) GetFeature() EStructuralFeature {
+	if list.owner != nil {
+		return list.owner.EClass().GetEStructuralFeature(list.featureID)
+	}
+	return nil
+}
+
+// GetFeatureID ...
+func (list *basicEDataTypeList) GetFeatureID() int {
+	return list.featureID
+}

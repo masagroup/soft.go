19685a98618438b38bd16a3201c02eef8c6e26b2
Jocelyn DUMAY <jocelyn@barry-dumay.com>
2022-05-06 11:16:24 +0200

    [ecore] add json encoder

--- ecore/jsoncodec.go
+++ ecore/jsoncodec.go
@@ -14,15 +14,59 @@ import (
 )
 
 const (
-	JSON_OPTION_ID_ATTRIBUTE = "ID_ATTRIBUTE" // if true, save id attribute of the object
+	JSON_OPTION_ID_ATTRIBUTE_NAME = "ID_ATTRIBUTE" // if true, save id attribute of the object
 )
 
 type JSONCodec struct {
 }
 
 func (jc *JSONCodec) NewEncoder(resource EResource, w io.Writer, options map[string]interface{}) EResourceEncoder {
-	return nil
+	return NewJSONEncoder(resource, w, options)
 }
 func (jc *JSONCodec) NewDecoder(resource EResource, r io.Reader, options map[string]interface{}) EResourceDecoder {
 	return nil
+}
+
+type jsonFeatureKind int
+
+const (
+	jfkTransient jsonFeatureKind = iota
+	jfkData
+	jfkDataList
+	jfkObject
+	jfkObjectList
+	jfkObjectReference
+	jfkObjectReferenceList
+)
+
+func getJSONCodecFeatureKind(eFeature EStructuralFeature) jsonFeatureKind {
+	if eFeature.IsTransient() {
+		return jfkTransient
+	} else if eReference, _ := eFeature.(EReference); eReference != nil {
+		opposite := eReference.GetEOpposite()
+		if opposite != nil && opposite.IsContainment() {
+			return jfkTransient
+		}
+		if eReference.IsResolveProxies() {
+			if eReference.IsMany() {
+				return jfkObjectReferenceList
+			} else {
+				return jfkObjectReference
+			}
+		} else {
+			if eReference.IsMany() {
+				return jfkObjectList
+			} else {
+				return jfkObject
+			}
+		}
+
+	} else if eAttribute, _ := eFeature.(EAttribute); eAttribute != nil {
+		if eAttribute.IsMany() {
+			return jfkDataList
+		} else {
+			return jfkData
+		}
+	}
+	return -1
 }
--- ecore/jsonencoder.go
+++ ecore/jsonencoder.go
@@ -7,12 +7,12 @@ import (
 )
 
 type JSONEncoder struct {
-	w                    *jsonwriter.Writer
-	resource             EResource
-	objectRoot           EObject
-	baseURI              *URI
-	isIDAttributeEncoded bool
-	errorFn              func(diagnostic EDiagnostic)
+	w               *jsonwriter.Writer
+	resource        EResource
+	idAttributeName string
+	keepDefaults    bool
+	featureKinds    map[EStructuralFeature]jsonFeatureKind
+	errorFn         func(diagnostic EDiagnostic)
 }
 
 func NewJSONEncoder(resource EResource, w io.Writer, options map[string]interface{}) *JSONEncoder {
@@ -20,11 +20,8 @@ func NewJSONEncoder(resource EResource, w io.Writer, options map[string]interfac
 		w:        jsonwriter.New(w),
 		resource: resource,
 	}
-	if uri := resource.GetURI(); uri != nil {
-		e.baseURI = uri
-	}
 	if options != nil {
-		e.isIDAttributeEncoded = options[JSON_OPTION_ID_ATTRIBUTE] == true
+		e.idAttributeName, _ = options[JSON_OPTION_ID_ATTRIBUTE_NAME].(string)
 	}
 	return e
 }
@@ -51,66 +48,136 @@ func (e *JSONEncoder) EncodeObject(object EObject) (err error) {
 
 func (e *JSONEncoder) encodeTopObject(eObject EObject) {
 	e.w.RootObject(func() {
-		e.encodeObject(eObject, checkContainer)
+		e.encodeObject(eObject)
 	})
 }
 
-func (e *JSONEncoder) encodeObject(eObject EObject, check checkType) {
-	eObjectInternal := eObject.(EObjectInternal)
+func (e *JSONEncoder) encodeObject(eObject EObject) {
 	eClass := eObject.EClass()
-	ePackage := eClass.GetEPackage()
-	e.w.KeyString("eClass", ePackage.GetNsURI()+"#//"+eClass.GetName())
-	saveFeatureValues := true
-	switch check {
-	case checkDirectResource:
-		if eObjectInternal.EIsProxy() {
-			e.w.KeyString("eRef", eObjectInternal.EProxyURI().String())
-			saveFeatureValues = false
-		} else if eResource := eObjectInternal.EInternalResource(); eResource != nil {
-			uri := eResource.GetURI().Copy()
-			uri.Fragment = eResource.GetURIFragment(eObject)
-			e.w.KeyString("eRef", uri.String())
-			saveFeatureValues = false
-		}
-	case checkResource:
-		if eObjectInternal.EIsProxy() {
-			e.w.KeyString("eRef", eObjectInternal.EProxyURI().String())
-			saveFeatureValues = false
-		} else if eResource := eObjectInternal.EResource(); eResource != nil &&
-			(eResource != e.resource ||
-				(e.objectRoot != nil && !IsAncestor(e.objectRoot, eObjectInternal))) {
-			// encode object as uri and fragment if object is in a different resource
-			// or if in the same resource and root object is not its ancestor
-			uri := eResource.GetURI().Copy()
-			uri.Fragment = eResource.GetURIFragment(eObject)
-			e.w.KeyString("eRef", uri.String())
-			saveFeatureValues = false
-		}
-	case checkNothing:
-	case checkContainer:
-	}
-
-	if saveFeatureValues {
-
-		// id attribute
-		if objectIDManager := e.resource.GetObjectIDManager(); e.isIDAttributeEncoded && objectIDManager != nil {
-			if id := objectIDManager.GetID(eObject); id != nil {
-				e.w.KeyValue("eID", id)
-			}
-		}
-
-		// features
-		for itFeature := eClass.GetEAllStructuralFeatures().Iterator(); itFeature.HasNext(); {
-			eFeature := itFeature.Next().(EStructuralFeature)
-
-			if !eFeature.IsTransient() {
-				e.encodeFeatureValue(eObjectInternal, eFeature)
-			}
+	// class
+	e.w.KeyString("eClass", e.getClassName(eClass))
+	// id
+	if idManager := e.resource.GetObjectIDManager(); len(e.idAttributeName) > 0 && idManager != nil {
+		if id := idManager.GetID(eObject); id != nil {
+			e.w.KeyValue(e.idAttributeName, id)
 		}
 	}
-
+	// features
+	for itFeature := eClass.GetEAllStructuralFeatures().Iterator(); itFeature.HasNext(); {
+		eFeature := itFeature.Next().(EStructuralFeature)
+		e.encodeFeatureValue(eObject, eFeature)
+	}
 }
 
-func (e *JSONEncoder) encodeFeatureValue(eObject EObject, check checkType) {
+func (e *JSONEncoder) encodeObjectReference(eObject EObject) {
+	e.w.KeyString("eClass", e.getClassName(eObject.EClass()))
+	e.w.KeyString("eRef", e.getReference(eObject))
+}
 
+func (e *JSONEncoder) encodeFeatureValue(eObject EObject, eFeature EStructuralFeature) {
+	if !e.shouldSaveFeature(eObject, eFeature) {
+		return
+	}
+
+	// compute feature kind
+	kind, ok := e.featureKinds[eFeature]
+	if !ok {
+		kind = getJSONCodecFeatureKind(eFeature)
+		e.featureKinds[eFeature] = kind
+	}
+
+	value := eObject.EGetResolve(eFeature, false)
+	switch kind {
+	case jfkTransient:
+	case jfkData:
+		str, ok := e.getData(value, eFeature)
+		if ok {
+			e.w.KeyString(eFeature.GetName(), str)
+		}
+	case jfkDataList:
+		l := value.(EList)
+		e.w.Array(eFeature.GetName(), func() {
+			for it := l.Iterator(); it.HasNext(); {
+				str, ok := e.getData(it.Next(), eFeature)
+				if ok {
+					e.w.Value(str)
+				}
+			}
+		})
+	case jfkObject:
+		e.w.Object(eFeature.GetName(), func() {
+			e.encodeObject(value.(EObject))
+		})
+	case jfkObjectList:
+		l := value.(EList)
+		e.w.Array(eFeature.GetName(), func() {
+			for it := l.Iterator(); it.HasNext(); {
+				e.w.ArrayObject(func() {
+					e.encodeObject(it.Next().(EObject))
+				})
+			}
+		})
+	case jfkObjectReference:
+		e.w.Object(eFeature.GetName(), func() {
+			e.encodeObjectReference(value.(EObject))
+		})
+	case jfkObjectReferenceList:
+		l := value.(EList)
+		e.w.Array(eFeature.GetName(), func() {
+			for it := l.Iterator(); it.HasNext(); {
+				e.w.ArrayObject(func() {
+					e.encodeObjectReference(it.Next().(EObject))
+				})
+			}
+		})
+	}
+}
+
+func (e *JSONEncoder) shouldSaveFeature(o EObject, f EStructuralFeature) bool {
+	return o.EIsSet(f) || (e.keepDefaults && f.GetDefaultValueLiteral() != "")
+}
+
+func (e *JSONEncoder) getClassName(eClass EClass) string {
+	ePackage := eClass.GetEPackage()
+	return ePackage.GetNsURI() + "#//" + eClass.GetName()
+}
+
+func (e *JSONEncoder) getData(value interface{}, f EStructuralFeature) (string, bool) {
+	if value == nil {
+		return "", false
+	} else {
+		d := f.GetEType().(EDataType)
+		p := d.GetEPackage()
+		f := p.GetEFactoryInstance()
+		s := f.ConvertToString(d, value)
+		return s, true
+	}
+}
+
+func (e *JSONEncoder) getReference(eObject EObject) string {
+	eInternal, _ := eObject.(EObjectInternal)
+	if eInternal != nil {
+		objectURI := eInternal.EProxyURI()
+		if objectURI == nil {
+			eOtherResource := eObject.EResource()
+			if eOtherResource == nil {
+				if e.resource != nil && e.resource.GetObjectIDManager() != nil && e.resource.GetObjectIDManager().GetID(eObject) != nil {
+					objectURI = e.getResourceReference(e.resource, eObject)
+				} else {
+					return ""
+				}
+			} else {
+				objectURI = e.getResourceReference(eOtherResource, eObject)
+			}
+		}
+		objectURI = e.resource.GetURI().Relativize(objectURI)
+		return objectURI.String()
+	}
+	return ""
+}
+
+func (e *JSONEncoder) getResourceReference(resource EResource, object EObject) *URI {
+	uri := resource.GetURI().Copy()
+	uri.Fragment = resource.GetURIFragment(object)
+	return uri
 }

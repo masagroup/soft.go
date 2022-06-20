6f5c31a90d9d6264298bfa6a26318caf7bf56d97
Jocelyn DUMAY <jocelyn@barry-dumay.com>
2022-06-20 10:54:58 +0200

    [ecore] BasicEObjectMap tests with notifications

--- ecore/basiceobjectmap_test.go
+++ ecore/basiceobjectmap_test.go
@@ -18,7 +18,8 @@ import (
 
 func TestBasicEObjectMap_Constructor(t *testing.T) {
 	mockClass := &MockEClass{}
-	m := NewBasicEObjectMap(mockClass)
+	mockEObject := &MockEObjectInternal{}
+	m := NewBasicEObjectMap(mockClass, mockEObject, 1, -1, false)
 	assert.NotNil(t, m)
 
 	var mp EMap = m
@@ -37,8 +38,9 @@ func TestBasicEObjectMap_Put(t *testing.T) {
 	mockClass := &MockEClass{}
 	mockPackage := &MockEPackage{}
 	mockFactory := &MockEFactory{}
+	mockOwner := &MockEObjectInternal{}
 	mockEntry := &MockEObjectEMapEntry{}
-	m := NewBasicEObjectMap(mockClass)
+	m := NewBasicEObjectMap(mockClass, mockOwner, 1, -1, false)
 
 	mockClass.On("GetEPackage").Once().Return(mockPackage)
 	mockPackage.On("GetEFactoryInstance").Once().Return(mockFactory)
@@ -47,6 +49,32 @@ func TestBasicEObjectMap_Put(t *testing.T) {
 	mockEntry.On("SetValue", "2").Once()
 	mockEntry.On("GetKey").Once().Return(2)
 	mockEntry.On("GetValue").Once().Return("2")
+	mockOwner.On("EDeliver").Once().Return(false)
 	m.Put(2, "2")
-	mock.AssertExpectationsForObjects(t, mockClass, mockPackage, mockFactory, mockEntry)
+	mock.AssertExpectationsForObjects(t, mockClass, mockPackage, mockFactory, mockEntry, mockOwner)
+}
+
+func TestBasicEObjectMap_Put_WithNotification(t *testing.T) {
+	mockClass := &MockEClass{}
+	mockPackage := &MockEPackage{}
+	mockFactory := &MockEFactory{}
+	mockOwner := &MockEObjectInternal{}
+	mockEntry := &MockEObjectEMapEntry{}
+	mockAdapter := new(MockEAdapter)
+	m := NewBasicEObjectMap(mockClass, mockOwner, 1, -1, false)
+
+	mockClass.On("GetEPackage").Once().Return(mockPackage)
+	mockPackage.On("GetEFactoryInstance").Once().Return(mockFactory)
+	mockFactory.On("Create", mockClass).Once().Return(mockEntry)
+	mockEntry.On("SetKey", 2).Once()
+	mockEntry.On("SetValue", "2").Once()
+	mockEntry.On("GetKey").Once().Return(2)
+	mockEntry.On("GetValue").Once().Return("2")
+	mockOwner.On("EDeliver").Once().Return(true)
+	mockOwner.On("EAdapters").Return(NewImmutableEList([]interface{}{mockAdapter})).Once()
+	mockOwner.On("ENotify", mock.MatchedBy(func(n ENotification) bool {
+		return n.GetNotifier() == mockOwner && n.GetFeatureID() == 1 && n.GetEventType() == ADD && n.GetNewValue() == mockEntry
+	})).Once()
+	m.Put(2, "2")
+	mock.AssertExpectationsForObjects(t, mockClass, mockPackage, mockFactory, mockEntry, mockOwner)
 }

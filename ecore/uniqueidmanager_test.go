2417ea8bf4bda9afacd94bb8e6550f05b6c37abb
Jocelyn DUMAY <jocelyn@barry-dumay.com>
2022-11-02 18:36:53 +0100

    [ecore] move ULIDManager tests to UniqueIDManager tests

--- ecore/uniqueidmanager_test.go
+++ ecore/uniqueidmanager_test.go
@@ -122,3 +122,41 @@ func TestUUIDManagerUnRegister(t *testing.T) {
 	m.Register(mockObject)
 	assert.Equal(t, id, m.GetID(mockObject))
 }
+
+func TestULIDManagerRegister(t *testing.T) {
+	m := NewULIDManager()
+	mockObject := &MockEObject{}
+	mockOther := &MockEObject{}
+	m.Register(mockObject)
+
+	id := m.GetID(mockObject)
+	assert.NotNil(t, id)
+	assert.Nil(t, m.GetID(mockOther))
+
+	assert.Equal(t, mockObject, m.GetEObject(id))
+	assert.Nil(t, m.GetEObject(""))
+	assert.Nil(t, m.GetEObject(3))
+
+}
+
+func TestULIDManagerUnRegister(t *testing.T) {
+	m := NewULIDManager()
+
+	// register object
+	mockObject := &MockEObject{}
+	m.Register(mockObject)
+
+	id := m.GetID(mockObject)
+	assert.NotNil(t, id)
+	assert.Nil(t, m.GetDetachedID(mockObject))
+
+	// unregister
+	assert.False(t, m.KeepIDs(true))
+	m.UnRegister(mockObject)
+	assert.Nil(t, m.GetID(mockObject))
+	assert.Equal(t, id, m.GetDetachedID(mockObject))
+
+	// register again and check it was detached
+	m.Register(mockObject)
+	assert.Equal(t, id, m.GetID(mockObject))
+}

2eafcb646ffb99cfa9c12231dd134642dfd975c2
Jocelyn DUMAY <jocelyn@barry-dumay.com>
2023-05-25 16:12:56 +0200

    [ecore] test json encoder with expected files

--- ecore/jsonencoder_test.go
+++ ecore/jsonencoder_test.go
@@ -1,7 +1,9 @@
 package ecore
 
 import (
+	"bytes"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -20,12 +22,14 @@ func TestJSONEncoder_EncodeResourceSimple(t *testing.T) {
 	assert.True(t, eResource.IsLoaded())
 	assert.True(t, eResource.GetErrors().Empty(), diagnosticError(eResource.GetErrors()))
 
-	f, error := os.Create("testdata/library.simple.result.json")
-	require.Nil(t, error)
-	defer f.Close()
-
-	encoder := NewJSONEncoder(eResource, f, nil)
+	buffer := &bytes.Buffer{}
+	encoder := NewJSONEncoder(eResource, buffer, nil)
 	encoder.EncodeResource()
+
+	bytes, err := os.ReadFile("testdata/library.simple.json")
+	assert.Nil(t, err)
+	assert.Equal(t, strings.ReplaceAll(string(bytes), "\r\n", "\n"), strings.ReplaceAll(buffer.String(), "\r\n", "\n"))
+
 }
 
 func TestJSONEncoder_EncodeResourceComplex(t *testing.T) {
@@ -40,10 +44,11 @@ func TestJSONEncoder_EncodeResourceComplex(t *testing.T) {
 	assert.True(t, eResource.IsLoaded())
 	assert.True(t, eResource.GetErrors().Empty(), diagnosticError(eResource.GetErrors()))
 
-	f, error := os.Create("testdata/library.complex.result.json")
-	require.Nil(t, error)
-	defer f.Close()
-
-	encoder := NewJSONEncoder(eResource, f, nil)
+	buffer := &bytes.Buffer{}
+	encoder := NewJSONEncoder(eResource, buffer, nil)
 	encoder.EncodeResource()
+
+	bytes, err := os.ReadFile("testdata/library.complex.json")
+	assert.Nil(t, err)
+	assert.Equal(t, strings.ReplaceAll(string(bytes), "\r\n", "\n"), strings.ReplaceAll(buffer.String(), "\r\n", "\n"))
 }

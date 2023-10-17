a98da0d46031acc98b4ef9cf2b4a9cc0d2dc3ca6
Jocelyn DUMAY <jocelyn@barry-dumay.com>
2023-10-17 10:13:13 +0200

    [ecore] add SQLStore.Set with single value test

--- ecore/sqlstore_test.go
+++ ecore/sqlstore_test.go
@@ -3,6 +3,7 @@ package ecore
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -13,5 +14,30 @@ func TestSQLStore_Constructor(t *testing.T) {
 }
 
 func TestSQLStore_SetSingleValue(t *testing.T) {
+	xmiProcessor := NewXMIProcessor()
+	resource := xmiProcessor.Load(NewURI("testdata/library.complex.ecore"))
+	require.NotNil(t, resource)
+	assert.True(t, resource.IsLoaded())
+	assert.True(t, resource.GetErrors().Empty(), diagnosticError(resource.GetErrors()))
+	assert.True(t, resource.GetWarnings().Empty(), diagnosticError(resource.GetWarnings()))
 
+	contents := resource.GetContents()
+	require.Equal(t, 1, contents.Size())
+
+	ePackage, _ := contents.Get(0).(EPackage)
+	require.NotNil(t, ePackage)
+
+	eClass, _ := ePackage.GetEClassifier("Lendable").(EClass)
+	require.NotNil(t, eClass)
+
+	eFeature := eClass.GetEStructuralFeatureFromName("copies")
+	require.NotNil(t, eFeature)
+
+	s, err := NewSQLStore("testdata/library.store.sqlite", NewURI(""), nil, nil)
+	require.Nil(t, err)
+	require.NotNil(t, s)
+	mockObject := NewMockSQLObject(t)
+	mockObject.EXPECT().GetSqlID().Return(int64(3)).Once()
+	mockObject.EXPECT().EClass().Return(eClass).Once()
+	s.Set(mockObject, eFeature, -1, 5)
 }

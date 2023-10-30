ab1288928e9ef53c4714912b9d656b6e5f4fd74f
JDY <jocelyn.dumay@masagroup.net>
2023-10-30 17:46:39 +0100

    [ecore] SQLStore.Clear tests

--- ecore/sqlstore_test.go
+++ ecore/sqlstore_test.go
@@ -960,3 +960,39 @@ func TestSQLStore_Remove_NonExisting(t *testing.T) {
 	previous := s.Remove(mockObject, eBooksFeature, 2)
 	assert.Nil(t, previous)
 }
+
+func TestSQLStore_Clear(t *testing.T) {
+	ePackage := loadPackage("library.datalist.ecore")
+	require.NotNil(t, ePackage)
+
+	eClass, _ := ePackage.GetEClassifier("Book").(EClass)
+	require.NotNil(t, eClass)
+
+	eFeature := eClass.GetEStructuralFeatureFromName("contents")
+	require.NotNil(t, eFeature)
+
+	dbPath := filepath.Join(t.TempDir(), "library.datalist.sqlite")
+	err := copyFile("testdata/library.datalist.sqlite", dbPath)
+	require.Nil(t, err)
+	// create store
+	s, err := NewSQLStore(dbPath, NewURI(""), nil, nil, nil)
+	require.Nil(t, err)
+	require.NotNil(t, s)
+	defer s.Close()
+
+	mockObject := NewMockSQLObject(t)
+	mockObject.EXPECT().GetSqlID().Return(int64(5)).Once()
+	mockObject.EXPECT().EClass().Return(eClass).Once()
+	s.Clear(mockObject, eFeature)
+
+	// check store
+	db, err := sql.Open("sqlite", dbPath)
+	require.NoError(t, err)
+	require.NotNil(t, db)
+	defer db.Close()
+	var count int
+	row := db.QueryRow("SELECT COUNT(contents) FROM book_contents WHERE bookID=5")
+	err = row.Scan(&count)
+	assert.NoError(t, err)
+	assert.Equal(t, 0, count)
+}

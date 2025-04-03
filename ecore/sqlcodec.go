1e31603dd4ced466d99302b84797372f3d1af790
Jocelyn DUMAY <jocelyn@barry-dumay.com>
2025-04-03 14:57:34 +0200

    [ecore] add SQL_OPTION_WITH_OBJECTS option in sqldecoder

--- ecore/sqlcodec.go
+++ ecore/sqlcodec.go
@@ -18,6 +18,7 @@ const (
 	SQL_OPTION_SQL_ID_MANAGER     = "SQL_ID_MANAGER"     // SQL id manager
 	SQL_OPTION_IN_MEMORY_DATABASE = "IN_MEMORY_DATABASE" // uses a memory database
 	SQL_OPTION_WITH_FEATURES      = "WITH_FEATURES"      // with object features
+	SQL_OPTION_WITH_OBJECTS       = "WITH_OBJECTS"       // with objects
 	SQL_OPTION_LOGGER             = "LOGGER"
 )
 
--- ecore/sqldecoder.go
+++ ecore/sqldecoder.go
@@ -576,6 +576,7 @@ type SQLDecoder struct {
 	sqlDecoder
 	resource     EResource
 	withFeatures bool
+	withObjects  bool
 }
 
 const SQLITE_MAX_ALLOCATION_SIZE = 2147483391
@@ -749,6 +750,7 @@ func newSQLDecoder(connectionPoolProvider func() (*sqlitex.Pool, error), connect
 	sqlIDManager := newSQLDecoderIDManager()
 	logger := zap.NewNop()
 	withFeatures := true
+	withObjects := true
 	if options != nil {
 		if v, isVersion := options[SQL_OPTION_CODEC_VERSION].(int64); isVersion {
 			codecVersion = v
@@ -764,7 +766,9 @@ func newSQLDecoder(connectionPoolProvider func() (*sqlitex.Pool, error), connect
 		if b, isWithFeatures := options[SQL_OPTION_WITH_FEATURES].(bool); isWithFeatures {
 			withFeatures = b
 		}
-
+		if b, isWithObjects := options[SQL_OPTION_WITH_OBJECTS].(bool); isWithObjects {
+			withObjects = b
+		}
 	}
 
 	// package registry
@@ -798,6 +802,7 @@ func newSQLDecoder(connectionPoolProvider func() (*sqlitex.Pool, error), connect
 		},
 		resource:     resource,
 		withFeatures: withFeatures,
+		withObjects:  withObjects,
 	}
 }
 
@@ -840,9 +845,11 @@ func (d *SQLDecoder) DecodeResource() {
 		return
 	}
 
-	if err := d.decodeObjects(); err != nil {
-		d.addError(err)
-		return
+	if d.withObjects {
+		if err := d.decodeObjects(); err != nil {
+			d.addError(err)
+			return
+		}
 	}
 
 	if d.withFeatures {
@@ -868,9 +875,9 @@ func (d *SQLDecoder) decodeContents() error {
 		&sqlitex.ExecOptions{
 			ResultFunc: func(stmt *sqlite.Stmt) error {
 				objectID := stmt.ColumnInt64(0)
-				object, _ := d.sqlIDManager.GetObjectFromID(objectID)
-				if object == nil {
-					return fmt.Errorf("unable to find object with id '%v'", objectID)
+				object, err := d.decodeObject(objectID)
+				if err != nil {
+					return err
 				}
 				d.resource.GetContents().Add(object)
 				return nil
